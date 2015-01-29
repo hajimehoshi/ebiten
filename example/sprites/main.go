c3eb4e98c313e838d9f0097bb6759a5ff6575dbe
Hajime Hoshi <hajimehoshi@gmail.com>
2015-01-29 23:59:55 +0900

    example/sprites: Add UI to adjust the number of sprites (#87)

--- example/sprites/main.go
+++ example/sprites/main.go
@@ -64,31 +64,58 @@ func (s *Sprite) Update() {
 	}
 }
 
-type Sprites []*Sprite
+type Sprites struct {
+	sprites []*Sprite
+	num     int
+}
 
 func (s Sprites) Update() {
-	for _, sprite := range s {
+	for _, sprite := range s.sprites {
 		sprite.Update()
 	}
 }
 
 func (s Sprites) Len() int {
-	return len(s)
+	return s.num
 }
 
 func (s Sprites) Dst(i int) (x0, y0, x1, y1 int) {
-	ss := s[i]
+	if s.num <= i {
+		return 0, 0, 0, 0
+	}
+	ss := s.sprites[i]
 	return ss.x, ss.y, ss.x + ebitenImageWidth, ss.y + ebitenImageHeight
 }
 
 func (s Sprites) Src(i int) (x0, y0, x1, y1 int) {
+	if s.num <= i {
+		return 0, 0, 0, 0
+	}
 	return 0, 0, ebitenImageWidth, ebitenImageHeight
 }
 
-var sprites = make(Sprites, 10000)
+const (
+	MinSprites = 0
+	MaxSprites = 10000
+)
+
+var sprites = &Sprites{make([]*Sprite, MaxSprites), 500}
 
 func update(screen *ebiten.Image) error {
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		sprites.num -= 20
+		if sprites.num < MinSprites {
+			sprites.num = MinSprites
+		}
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		sprites.num += 20
+		if MaxSprites < sprites.num {
+			sprites.num = MaxSprites
+		}
+	}
 	sprites.Update()
+
 	op := &ebiten.DrawImageOptions{
 		ImageParts: sprites,
 	}
@@ -96,7 +123,10 @@ func update(screen *ebiten.Image) error {
 	if err := screen.DrawImage(ebitenImage, op); err != nil {
 		return err
 	}
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f\nNum of sprites: %d", ebiten.CurrentFPS(), sprites.Len()))
+	msg := fmt.Sprintf(`FPS: %0.2f
+Num of sprites: %d
+Press <- or -> to change the number of sprites`, ebiten.CurrentFPS(), sprites.Len())
+	ebitenutil.DebugPrint(screen, msg)
 	return nil
 }
 
@@ -107,11 +137,11 @@ func main() {
 		log.Fatal(err)
 	}
 	ebitenImageWidth, ebitenImageHeight = ebitenImage.Size()
-	for i, _ := range sprites {
+	for i, _ := range sprites.sprites {
 		w, h := ebitenImage.Size()
 		x, y := rand.Intn(screenWidth-w), rand.Intn(screenHeight-h)
 		vx, vy := 2*rand.Intn(2)-1, 2*rand.Intn(2)-1
-		sprites[i] = &Sprite{
+		sprites.sprites[i] = &Sprite{
 			image: ebitenImage,
 			x:     x,
 			y:     y,
