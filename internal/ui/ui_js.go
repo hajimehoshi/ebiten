8c2301e542a2924fa7ccacbfd3f3422f7a3035dd
Hajime Hoshi <hajimehoshi@gmail.com>
2015-02-16 02:49:17 +0900

    Fix misspelling

--- internal/ui/ui_js.go
+++ internal/ui/ui_js.go
@@ -23,7 +23,7 @@ import (
 )
 
 func Now() int64 {
-	// time.Now() is not reliable until GopherJS suports performance.now().
+	// time.Now() is not reliable until GopherJS supports performance.now().
 	return int64(js.Global.Get("performance").Call("now").Float() * float64(time.Millisecond))
 }
 
