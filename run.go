94f1e7df642e0c47c423fba7ea82f424cf49042a
Hajime Hoshi <hajimehoshi@gmail.com>
2016-02-28 03:08:22 +0900

    ui: Bug fix: Don't call ui.DoEvents too often

--- run.go
+++ run.go
@@ -109,12 +109,8 @@ func Run(f func(*Image) error, width, height, scale int, title string) error {
 			c := float64(now-beforeForUpdate) * 60 / float64(time.Second)
 			runContext.isRunningSlowly = c >= 2.5
 			for i := 0; i < int(c); i++ {
-				if err := ui.DoEvents(); err != nil {
-					return err
-				}
-				if ui.IsClosed() {
-					return nil
-				}
+				// TODO: Input should be updated here,
+				// but JavaScript version of ui.DoEvents does vsync.
 				if err := graphicsContext.update(f); err != nil {
 					return err
 				}
