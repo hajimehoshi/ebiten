5e892b3887e25848dbb7fa24a9d46391676cb5c9
Hajime Hoshi <hajimehoshi@gmail.com>
2016-07-01 18:27:01 +0900

    graphics: Add a comment about bug #211

--- image.go
+++ image.go
@@ -186,6 +186,8 @@ func (i *Image) Dispose() error {
 // This function may be slow (as for implementation, this calls glTexSubImage2D).
 //
 // This function is concurrent-safe.
+//
+// BUG(hajimehoshi) This might not work on Android (#211).
 func (i *Image) ReplacePixels(p []uint8) error {
 	return i.impl.ReplacePixels(p)
 }
