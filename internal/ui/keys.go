de1bb1b3920ebfd86ea924e5757e7743f235dd9f
Hajime Hoshi <hajimehoshi@gmail.com>
2016-08-27 00:50:26 +0900

    go generate

--- internal/ui/keys.go
+++ internal/ui/keys.go
@@ -1,4 +1,4 @@
-// Copyright 2013 Hajime Hoshi
+// Copyright 2013 The Ebiten Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
