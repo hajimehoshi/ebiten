f00729224cded6faa77e31c9eef8a9f48a849a40
Hajime Hoshi <hajimehoshi@gmail.com>
2016-08-29 09:51:38 +0900

    doc: Separate Google Analytics snippet in another file

---
+++ docs/scripts/googleanalytics.js
@@ -0,0 +1,14 @@
+(function() {
+    var location = window.location;
+    if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
+        return;
+    }
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-83252440-1', 'auto');
+    ga('send', 'pageview');
+})();
