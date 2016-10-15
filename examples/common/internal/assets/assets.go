23b2d086ee0d3a77403f704a1b5498de6887d382
Hajime Hoshi <hajimehoshi@gmail.com>
2016-10-16 00:09:03 +0900

    example: Add example tags

--- examples/common/internal/assets/assets.go
+++ examples/common/internal/assets/assets.go
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate go-bindata -nocompress -pkg=assets arcadefont.png
+// +build example
+
+//go:generate go-bindata -nocompress -pkg=assets -tags=example arcadefont.png
 //go:generate gofmt -s -w .
 
 package assets
