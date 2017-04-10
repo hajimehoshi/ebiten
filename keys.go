12f14e514016bfcf53d5c5386c982b114209e42f
Hajime Hoshi <hajimehoshi@gmail.com>
2017-04-10 10:45:04 +0900

    doc: Add comments for keys

--- keys.go
+++ keys.go
@@ -21,6 +21,8 @@ import (
 )
 
 // A Key represents a keyboard key.
+// These keys represent pysical keys of US keyboard.
+// For example, KeyQ represents Q key on US keybaords and ' (quote) key on Dvorak keyboards.
 type Key int
 
 // Keys
