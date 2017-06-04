3b9e6c27087aa088a7b14aa279bfab59b18989c8
Hajime Hoshi <hajimehoshi@gmail.com>
2017-06-04 21:09:42 +0900

    audio: Use the new oto API

--- audio/audio.go
+++ audio/audio.go
@@ -220,8 +220,9 @@ func (c *Context) Update() error {
 	// is unexpectable.
 	// e.g. a variable for JVM on Android might not be set.
 	if c.driver == nil {
-		// TODO: Rename this other than player
-		p, err := oto.NewPlayer(c.sampleRate, channelNum, bytesPerSample)
+		// The buffer size is 1/20 sec.
+		s := c.sampleRate * channelNum * bytesPerSample / 20
+		p, err := oto.NewPlayer(c.sampleRate, channelNum, bytesPerSample, s)
 		c.driver = p
 		if err != nil {
 			return err
