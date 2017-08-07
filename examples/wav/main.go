6377ee5006a9ad67d4560cc026c90bb11da5a9d9
Hajime Hoshi <hajimehoshi@gmail.com>
2017-08-08 02:04:37 +0900

    Add examples/wav

---
+++ examples/wav/main.go
@@ -0,0 +1,85 @@
+// Copyright 2017 The Ebiten Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build example
+
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/audio"
+	"github.com/hajimehoshi/ebiten/audio/wav"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+const (
+	screenWidth  = 320
+	screenHeight = 240
+	sampleRate   = 44100
+)
+
+var (
+	audioContext *audio.Context
+	audioPlayer  *audio.Player
+)
+
+func init() {
+	var err error
+	audioContext, err = audio.NewContext(sampleRate)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := ebitenutil.OpenFile("_resources/audio/jab.wav")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d, err := wav.Decode(audioContext, f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	audioPlayer, err = audio.NewPlayer(audioContext, d)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func update(screen *ebiten.Image) error {
+	if ebiten.IsKeyPressed(ebiten.KeyP) && !audioPlayer.IsPlaying() {
+		audioPlayer.Rewind()
+		audioPlayer.Play()
+	}
+	if err := audioContext.Update(); err != nil {
+		return err
+	}
+	if ebiten.IsRunningSlowly() {
+		return nil
+	}
+	if audioPlayer.IsPlaying() {
+		ebitenutil.DebugPrint(screen, "Bump!")
+	} else {
+		ebitenutil.DebugPrint(screen, "Press P to play the wav")
+	}
+	return nil
+}
+
+func main() {
+	if err := ebiten.Run(update, screenWidth, screenHeight, 2, "WAV (Ebiten Demo)"); err != nil {
+		log.Fatal(err)
+	}
+}
