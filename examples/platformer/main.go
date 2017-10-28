3a3c15b1156f5e8f403a60235e0bd0730a14ec74
Floppy <voidcrystal200@gmail.com>
2017-10-28 15:21:49 +0200

    Added requests for pull request

--- examples/platformer/main.go
+++ examples/platformer/main.go
@@ -1,3 +1,19 @@
+// Copyright 2017 The Ebiten Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build example
+
 package main
 
 import (
@@ -8,79 +24,93 @@ import (
 
 const (
 	// Settings
-	width           int  = 1024
-	height          int  = 512
-	fullscreen      bool = false
-	runinbackground bool = true
+	width           = 1024
+	height          = 512
+	fullscreen      = false
+	runinbackground = true
 )
 
 var (
-	LoadedSprite    *ebiten.Image
-	LeftSprite      *ebiten.Image
-	RightSprite     *ebiten.Image
-	IdleSprite      *ebiten.Image
-	BackgroundImage *ebiten.Image
+	loadedSprite    *ebiten.Image
+	leftSprite      *ebiten.Image
+	rightSprite     *ebiten.Image
+	idleSprite      *ebiten.Image
+	backgroundImage *ebiten.Image
+	err             error
 
-	IsFirstFrame bool = true
+	isFirstFrame bool = true
 
-	BackgroundOptions = &ebiten.DrawImageOptions{}
-	CharSpriteOptions = &ebiten.DrawImageOptions{}
+	backgroundOptions = &ebiten.DrawImageOptions{}
+	charSpriteOptions = &ebiten.DrawImageOptions{}
 )
 
 func update(screen *ebiten.Image) error {
 	// Draws Background Image
-	screen.DrawImage(BackgroundImage, BackgroundOptions)
+	screen.DrawImage(backgroundImage, backgroundOptions)
 
 	// Resets
 	charX := 0.0
 	charY := 0.0
 
-	// I'm doing this to easily define a starting position for the gopher
-	if IsFirstFrame == true {
-		charY = charY + 390
+	if isFirstFrame == true {
+		charY = charY + 380
 		charX = charX + 50
-		IsFirstFrame = false
+		isFirstFrame = false
 	}
 
 	// Controls
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
 		// Selects preloaded sprite
-		LoadedSprite = LeftSprite
+		loadedSprite = leftSprite
 		// Moves character 3px right
 		charX = charX - 3.0
 	} else if ebiten.IsKeyPressed(ebiten.KeyD) {
 		// Selects preloaded sprite
-		LoadedSprite = RightSprite
+		loadedSprite = rightSprite
 		// Moves character 3px left
 		charX = charX + 3.0
 	} else {
-		LoadedSprite = IdleSprite
+		loadedSprite = idleSprite
 	}
 
 	// Change gopher's position
-	CharSpriteOptions.GeoM.Translate(charX, charY)
+	charSpriteOptions.GeoM.Translate(charX, charY)
 
 	// FPS counter
 	fps := fmt.Sprintf("FPS: %f", ebiten.CurrentFPS())
 	ebitenutil.DebugPrint(screen, fps)
 
 	// Draws selected sprite image
-	screen.DrawImage(LoadedSprite, CharSpriteOptions)
+	screen.DrawImage(loadedSprite, charSpriteOptions)
 
 	return nil
 }
 
 func main() {
 	// Settings for images
-	BackgroundOptions.GeoM.Scale(0.5, 0.5)
-	BackgroundOptions.GeoM.Translate(0, 0)
-	CharSpriteOptions.GeoM.Scale(0.5, 0.5)
+	backgroundOptions.GeoM.Scale(0.5, 0.5)
+	backgroundOptions.GeoM.Translate(0, 0)
+	charSpriteOptions.GeoM.Scale(0.5, 0.5)
 
-	// Preload images, This isn't proper error handling however has been kept short for simplicity purposes
-	RightSprite, _, _ = ebitenutil.NewImageFromFile("img/right.png", ebiten.FilterNearest)
-	LeftSprite, _, _ = ebitenutil.NewImageFromFile("img/left.png", ebiten.FilterNearest)
-	IdleSprite, _, _ = ebitenutil.NewImageFromFile("img/mainchar.png", ebiten.FilterNearest)
-	BackgroundImage, _, _ = ebitenutil.NewImageFromFile("img/background.png", ebiten.FilterNearest)
+	// Preload images
+	// Credit goes to Renee French for the gopher pictures
+	rightSprite, _, err = ebitenutil.NewImageFromFile("_resources/images/platformer/right.png", ebiten.FilterNearest)
+	if err != nil {
+		panic(err)
+	}
+	leftSprite, _, err = ebitenutil.NewImageFromFile("_resources/images/platformer/left.png", ebiten.FilterNearest)
+	if err != nil {
+		panic(err)
+	}
+	idleSprite, _, err = ebitenutil.NewImageFromFile("_resources/images/platformer/mainchar.png", ebiten.FilterNearest)
+	if err != nil {
+		panic(err)
+	}
+	// Background by CorvusSG
+	backgroundImage, _, err = ebitenutil.NewImageFromFile("_resources/images/platformer/background.png", ebiten.FilterNearest)
+	if err != nil {
+		panic(err)
+	}
 
 	ebiten.SetRunnableInBackground(runinbackground)
 	ebiten.SetFullscreen(fullscreen)
