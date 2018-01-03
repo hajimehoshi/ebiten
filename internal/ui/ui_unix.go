9ec0785203355a1afbfcbc61ff13726dca732ba0
Hajime Hoshi <hajimehoshi@gmail.com>
2018-01-03 23:32:21 +0900

    Rename files _xwindow.go to _unix.go

--- internal/ui/ui_unix.go
+++ internal/ui/ui_unix.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build freebsd linux
+// +build dragonfly freebsd linux netbsd openbsd solaris
 // +build !js
 // +build !android
 
