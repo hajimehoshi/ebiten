ccdc049ebb3f56314bfcc423579e85e33f5fbe21
Hajime Hoshi <hajimehoshi@gmail.com>
2018-03-14 00:53:08 +0900

    examples/keyboard/keyboard: Remove unused build tags

--- examples/keyboard/keyboard/generate.go
+++ examples/keyboard/keyboard/generate.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build example
-
 //go:generate go run -tags=example gen.go
 //go:generate gofmt -s -w .
 
