93b2fab95b38e7e40be52aa01301dc53b98a4834
Hajime Hoshi <hajimehoshi@gmail.com>
2018-03-16 12:05:53 +0900

    examples: Add comment to explain about files

--- examples/mosaic/main.go
+++ examples/mosaic/main.go
@@ -39,6 +39,15 @@ var (
 )
 
 func init() {
+	// Decode image from a byte slice instead of a file so that
+	// this example works in any working directory.
+	// If you want to use a file, there are some options:
+	// 1) Use os.Open and pass the file to the image decoder.
+	//    This is a very regular way, but doesn't work on browsers.
+	// 2) Use ebitenutil.OpenFile and pass the file to the image decoder.
+	//    This works even on browsers.
+	// 3) Use ebitenutil.NewImageFromFile to create an ebiten.Image directly from a file.
+	//    This also works on browsers.
 	img, _, err := image.Decode(bytes.NewReader(images.Gophers_jpg))
 	if err != nil {
 		log.Fatal(err)
