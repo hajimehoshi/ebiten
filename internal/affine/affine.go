2b4785f863ef77f7e67c23a39cddeae03b8143dd
Hajime Hoshi <hajimehoshi@gmail.com>
2018-07-27 01:23:14 +0900

    affine: Remove unused function

--- internal/affine/affine.go
+++ internal/affine/affine.go
@@ -14,17 +14,6 @@
 
 package affine
 
-// add is deprecated
-func add(lhs, rhs []float64, dim int) []float64 {
-	result := make([]float64, len(lhs))
-	for i := 0; i < dim-1; i++ {
-		for j := 0; j < dim; j++ {
-			result[i*dim+j] = lhs[i*dim+j] + rhs[i*dim+j]
-		}
-	}
-	return result
-}
-
 func mulSquare(lhs, rhs []float32, dim int) []float32 {
 	result := make([]float32, len(lhs))
 	for i := 0; i < dim; i++ {
