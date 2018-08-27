fe4f12fa6fcaf0bf31c85768164ef5f0ccc1fb8e
Hajime Hoshi <hajimehoshi@gmail.com>
2018-08-27 23:42:17 +0900

    text: Fix style

--- text/text_test.go
+++ text/text_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	"github.com/hajimehoshi/bitmapfont"
+
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/internal/testflock"
 	. "github.com/hajimehoshi/ebiten/text"
