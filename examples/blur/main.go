66ca53529be5bfc51a482718a8de7f40b17f662c
Hajime Hoshi <hajimehoshi@gmail.com>
2018-09-30 18:53:38 +0900

    examples: Add blur example

---
+++ examples/blur/main.go
@@ -0,0 +1,81 @@
+// Copyright 2018 The Ebiten Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build example jsgo
+
+package main
+
+import (
+	"bytes"
+	"image"
+	_ "image/jpeg"
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/examples/resources/images"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
+
+var (
+	gophersImage *ebiten.Image
+)
+
+func update(screen *ebiten.Image) error {
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(0, 0)
+	screen.DrawImage(gophersImage, op)
+
+	// Simple linear blur (7x7)
+	for j := -3; j <= 3; j++ {
+		for i := -3; i <= 3; i++ {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64(i), 244+float64(j))
+			// Alpha scale should be 1.0/49.0, but accumulating 1/49 49 times doesn't reach to 1 due to
+			// errors.
+			op.ColorM.Scale(1, 1, 1, 1.0/25.0)
+			screen.DrawImage(gophersImage, op)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	// Decode image from a byte slice instead of a file so that
+	// this example works in any working directory.
+	// If you want to use a file, there are some options:
+	// 1) Use os.Open and pass the file to the image decoder.
+	//    This is a very regular way, but doesn't work on browsers.
+	// 2) Use ebitenutil.OpenFile and pass the file to the image decoder.
+	//    This works even on browsers.
+	// 3) Use ebitenutil.NewImageFromFile to create an ebiten.Image directly from a file.
+	//    This also works on browsers.
+	img, _, err := image.Decode(bytes.NewReader(images.FiveYears_jpg))
+	if err != nil {
+		log.Fatal(err)
+	}
+	gophersImage, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+
+	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "Blur (Ebiten Demo)"); err != nil {
+		log.Fatal(err)
+	}
+}
