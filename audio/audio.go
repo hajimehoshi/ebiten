e6756efe5209ebc3e4d5fbdd3cbf33208290855d
Hajime Hoshi <hajimehoshi@gmail.com>
2018-12-17 23:06:56 +0900

    audio: Use a dummy driver for testing
    Fixes #750
    
    This also fixes an out-of-index bug when all players are skipped.

--- audio/audio.go
+++ audio/audio.go
@@ -74,15 +74,22 @@ func (p *players) Read(b []byte) (int, error) {
 	l := len(b)
 	l &= mask
 
+	allSkipped := true
+
 	for player := range p.players {
 		if player.shouldSkip() {
 			continue
 		}
+		allSkipped = false
 		s := player.bufferSizeInBytes()
 		if l > s {
 			l = s
 			l &= mask
 		}
+	}
+
+	if allSkipped {
+		l = 0
 	}
 
 	if l == 0 {
@@ -233,6 +240,15 @@ func CurrentContext() *Context {
 	return c
 }
 
+var driverForTesting io.WriteCloser
+
+func newDriver(sampleRate int) (io.WriteCloser, error) {
+	if driverForTesting != nil {
+		return driverForTesting, nil
+	}
+	return oto.NewPlayer(sampleRate, channelNum, bytesPerSample/channelNum, bufferSize())
+}
+
 func (c *Context) loop() {
 	initCh := make(chan struct{})
 
@@ -255,7 +271,7 @@ func (c *Context) loop() {
 	// e.g. a variable for JVM on Android might not be set.
 	<-initCh
 
-	p, err := oto.NewPlayer(c.sampleRate, channelNum, bytesPerSample/channelNum, bufferSize())
+	p, err := newDriver(c.sampleRate)
 	if err != nil {
 		c.err = err
 		return
