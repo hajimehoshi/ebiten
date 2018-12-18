ac06c804555c1628ecf3ec65ca4d1908f733f047
Hajime Hoshi <hajimehoshi@gmail.com>
2018-12-18 23:16:06 +0900

    ui: Bug fix: SetWindowIcon before Run didn't work
    Fixes #755

--- internal/ui/ui_glfw.go
+++ internal/ui/ui_glfw.go
@@ -106,10 +106,6 @@ func initialize() error {
 
 	currentUI.window.MakeContextCurrent()
 
-	if i := currentUI.getInitIconImages(); i != nil {
-		currentUI.window.SetIcon(i)
-	}
-
 	currentUI.window.SetInputMode(glfw.StickyMouseButtonsMode, glfw.True)
 	currentUI.window.SetInputMode(glfw.StickyKeysMode, glfw.True)
 	return nil
@@ -521,6 +517,10 @@ func Run(width, height int, scale float64, title string, g GraphicsContext, main
 			mode = glfw.CursorHidden
 		}
 		u.window.SetInputMode(glfw.CursorMode, mode)
+
+		if i := currentUI.getInitIconImages(); i != nil {
+			currentUI.window.SetIcon(i)
+		}
 
 		// Get the monitor before showing the window.
 		//
