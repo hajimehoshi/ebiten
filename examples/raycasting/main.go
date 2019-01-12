b2a7eae2f35289f9ba824a7bb13f59524d696b24
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2019-01-12 15:27:36 +0100

    example/shadow-raycasting: change ray length to untyped

--- examples/raycasting/main.go
+++ examples/raycasting/main.go
@@ -111,7 +111,7 @@ func calcAngle(l Line) float64 {
 func rayCasting(cx, cy float64, objects [][]Line) []Line {
 	var rays []Line
 
-	rayLength := 1000.0 // something large
+	rayLength := 1000 // something large
 	for _, obj := range objects {
 
 		// Get one of the endpoints for all segments,
