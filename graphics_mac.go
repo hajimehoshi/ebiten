f008c6ab808d40220067fc870592252843efc130
Hajime Hoshi <hajimehoshi@gmail.com>
2019-06-24 12:54:02 +0900

    graphics: Fix comments

--- graphics_mac.go
+++ graphics_mac.go
@@ -41,7 +41,7 @@ var (
 	// isMetalSupported represents whether Metal is supported.
 	isMetalSupported = true
 
-	// isMetalSupportedOnce initialized isMetalSupported.
+	// isMetalSupportedOnce initializs isMetalSupported.
 	//
 	// Use sync.Once instead of init function to avoid init-order dependency (#886).
 	isMetalSupportedOnce sync.Once
