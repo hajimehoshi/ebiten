31f56efd754ad8164e0a3203bf9e414829c94adb
Hajime Hoshi <hajimehoshi@gmail.com>
2019-10-28 01:28:46 +0900

    jsutil: Add doc.go
    This is to avoid no-Go-files error on TravisCI.
    
    https://travis-ci.org/hajimehoshi/ebiten/builds/603539948

---
+++ internal/jsutil/doc.go
@@ -0,0 +1,19 @@
+// Copyright 2019 The Ebiten Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This can be compiled in non-JS environments to avoid a mysterious error: 'no Go source files'
+// See https://travis-ci.org/hajimehoshi/ebiten/builds/603539948
+
+// Package jsutil offers utility functions for GopherJS and Wasm.
+package jsutil
