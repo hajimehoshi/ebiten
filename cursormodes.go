48453435c9693767f7d39076f58b44ffafd6aae3
Zachary Burkett <zburkett@splitcubestudios.com>
2019-12-12 16:29:15 -0500

    Fixed license typo

--- cursormodes.go
+++ cursormodes.go
@@ -1,4 +1,4 @@
-// Copyright 209 The Ebiten Authors
+// Copyright 2019 The Ebiten Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
