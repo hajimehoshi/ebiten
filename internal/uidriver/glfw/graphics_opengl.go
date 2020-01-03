fa95275a025cd387a3421c17ecc044fbd23baaa0
Hajime Hoshi <hajimehoshi@gmail.com>
2020-01-03 19:01:37 +0900

    uidriver/glfw: Bug fix: js should be excluded from compiling targets

--- internal/uidriver/glfw/graphics_opengl.go
+++ internal/uidriver/glfw/graphics_opengl.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build freebsd js linux windows ebitengl
+// +build freebsd linux windows ebitengl
+// +build !js
 
 package glfw
 
