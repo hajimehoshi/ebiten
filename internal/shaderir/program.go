6553c237a1c14d2879c1a55392b1d7ff7691ebea
Hajime Hoshi <hajimehoshi@gmail.com>
2020-05-16 23:42:32 +0900

    shaderir: Add comments

--- internal/shaderir/program.go
+++ internal/shaderir/program.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package shaderir offers intermediate representation for shader programs.
 package shaderir
 
 type Program struct {
