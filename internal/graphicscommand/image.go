968c0a9b9ab1b39ce8f7f6806e2f7d7f99ec1c5c
Hajime Hoshi <hajimehoshi@gmail.com>
2020-05-24 03:04:52 +0900

    graphicscommand: Bug fix: test failures

--- internal/graphicscommand/image.go
+++ internal/graphicscommand/image.go
@@ -145,7 +145,7 @@ func (i *Image) InternalSize() (int, int) {
 //
 // src and shader are exclusive and only either is non-nil.
 func (i *Image) DrawTriangles(src *Image, vertices []float32, indices []uint16, clr *affine.ColorM, mode driver.CompositeMode, filter driver.Filter, address driver.Address, shader *Shader, uniforms map[int]interface{}) {
-	if src.screen {
+	if src != nil && src.screen {
 		panic("graphicscommand: the screen image cannot be the rendering source")
 	}
 
