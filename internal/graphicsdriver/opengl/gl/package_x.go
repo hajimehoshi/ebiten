eb1e2802a298993e37df7c3876b42be1a1ec0666
Hajime Hoshi <hajimehoshi@gmail.com>
2020-07-23 17:30:29 +0900

    tmp

---
+++ internal/graphicsdriver/opengl/gl/package_x.go
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+// +build !js
+// +build freebsd linux
+
+package gl
+
+// #include <GL/glx.h>
+//
+// static const char* RendererDeviceString() {
+// #ifdef GLX_MESA_query_renderer
+//   static PFNGLXQUERYCURRENTRENDERERSTRINGMESAPROC queryString;
+//   if (!queryString) {
+//     queryString = (PFNGLXQUERYCURRENTRENDERERSTRINGMESAPROC)
+//       glXGetProcAddressARB((const GLubyte *)"glXQueryCurrentRendererStringMESA");
+//   }
+//
+//   static const char* rendererDevice;
+//   if (!rendererDevice) {
+//     rendererDevice = queryString(GLX_RENDERER_VENDOR_ID_MESA);
+//   }
+//
+//   return rendererDevice;
+// #else
+//   return "";
+// #endif
+// }
+import "C"
+
+func RendererDeviceString() string {
+	return C.GoString(C.RendererDeviceString())
+}
