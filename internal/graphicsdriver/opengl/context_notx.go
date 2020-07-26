70bfa32c0b985861f60775dd97c7c992d23d3a85
Hajime Hoshi <hajimehoshi@gmail.com>
2020-07-26 17:01:09 +0900

    graphicsdriver/opengl: Bug fix: Build error on Android
    Fixes #1266

--- internal/graphicsdriver/opengl/context_notx.go
+++ internal/graphicsdriver/opengl/context_notx.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build js !freebsd,!linux
+// +build android js !freebsd,!linux
 
 package opengl
 
