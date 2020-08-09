5dc0d4e1a45953347966c01c6998c8ef24edb76a
Hajime Hoshi <hajimehoshi@gmail.com>
2020-08-09 22:26:57 +0900

    shader: Add more tests about 'for' loops

---
+++ internal/shader/testdata/for3.go
@@ -0,0 +1,15 @@
+package main
+
+func Foo() vec2 {
+	v := vec2(0)
+	for i := 0; i < 100; i++ {
+		v2 := vec2(0)
+		v = v2
+	}
+	v3 := vec2(0)
+	for i := 0; i < 100; i++ {
+		v4 := vec2(0)
+		v3 = v4
+	}
+	return v
+}
