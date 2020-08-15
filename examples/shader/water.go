a1d278b097d858690acf3798328bc073fe7e4372
Hajime Hoshi <hajimehoshi@gmail.com>
2020-08-15 21:36:10 +0900

    examples/shader: Add water.go
    Updates #1284

---
+++ examples/shader/water.go
@@ -0,0 +1,42 @@
+// Copyright 2020 The Ebiten Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build ignore
+
+package main
+
+var Time float
+var Cursor vec2
+var ScreenSize vec2
+
+func Fragment(position vec4, texCoord vec2, color vec4) vec4 {
+	border := ScreenSize.y*0.6 + 4*cos(Time*3+texCoord.y*200)
+	if position.y < border {
+		return image2TextureAt(texCoord)
+	}
+
+	srcsize := imageSrcTextureSize()
+	rorigin, rsize := imageSrcTextureSourceRegion()
+
+	xoffset := (4 / srcsize.x) * cos(Time*3+texCoord.y*200)
+	yoffset := (20 / srcsize.y) * (1.0 + cos(Time*3+texCoord.y*50))
+	bordertex := border / srcsize.y
+	clr := vec4(image2TextureBoundsAt(vec2(
+		texCoord.x+xoffset,
+		-(texCoord.y+yoffset-rorigin.y)+bordertex*2+rorigin.y,
+	)))
+
+	overlay := vec4(0.5, 1, 1, 1)
+	return mix(clr, overlay, 0.25)
+}
