b3afcae16ccfe411250686e50943d1a111c2ab4f
Hajime Hoshi <hajimehoshi@gmail.com>
2020-10-17 22:36:07 +0900

    examples/shader: Add build tags for the generated files
    Closes #1581

--- examples/shader/generate.go
+++ examples/shader/generate.go
@@ -12,11 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build example
+
 package main
 
-//go:generate file2byteslice -package=main -input=default.go -output=default_go.go -var=default_go
-//go:generate file2byteslice -package=main -input=lighting.go -output=lighting_go.go -var=lighting_go
-//go:generate file2byteslice -package=main -input=radialblur.go -output=radialblur_go.go -var=radialblur_go
-//go:generate file2byteslice -package=main -input=chromaticaberration.go -output=chromaticaberration_go.go -var=chromaticaberration_go
-//go:generate file2byteslice -package=main -input=dissolve.go -output=dissolve_go.go -var=dissolve_go
-//go:generate file2byteslice -package=main -input=water.go -output=water_go.go -var=water_go
+//go:generate file2byteslice -package=main -input=default.go -output=default_go.go -var=default_go -buildtags=example
+//go:generate file2byteslice -package=main -input=lighting.go -output=lighting_go.go -var=lighting_go -buildtags=example
+//go:generate file2byteslice -package=main -input=radialblur.go -output=radialblur_go.go -var=radialblur_go -buildtags=example
+//go:generate file2byteslice -package=main -input=chromaticaberration.go -output=chromaticaberration_go.go -var=chromaticaberration_go -buildtags=example
+//go:generate file2byteslice -package=main -input=dissolve.go -output=dissolve_go.go -var=dissolve_go -buildtags=example
+//go:generate file2byteslice -package=main -input=water.go -output=water_go.go -var=water_go -buildtags=example
