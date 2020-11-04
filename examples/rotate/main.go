61a8f2c69c115ccc2d0dbbe980d31c33d7f3e195
Hajime Hoshi <hajimehoshi@gmail.com>
2020-11-05 00:37:24 +0900

    examples/rotate: Update the resolution

--- examples/rotate/main.go
+++ examples/rotate/main.go
@@ -28,8 +28,8 @@ import (
 )
 
 const (
-	screenWidth  = 320
-	screenHeight = 240
+	screenWidth  = 640
+	screenHeight = 480
 )
 
 var (
@@ -84,7 +84,7 @@ func main() {
 	}
 	gophersImage = ebiten.NewImageFromImage(img)
 
-	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Rotate (Ebiten Demo)")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
