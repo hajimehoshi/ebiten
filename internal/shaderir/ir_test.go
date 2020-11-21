b72848dc97b4cbf10b852cc1b5f62f498d926d80
Hajime Hoshi <hajimehoshi@gmail.com>
2020-11-21 19:17:24 +0900

    shaderir: Bug fix: Test failures
    Updates #1404

--- internal/shaderir/ir_test.go
+++ internal/shaderir/ir_test.go
@@ -152,7 +152,7 @@ func fieldSelectorExpr(a, b Expr) Expr {
 }
 
 func TestOutput(t *testing.T) {
-	glslPrelude := glsl.FragmentPrelude + "\n"
+	glslPrelude := glsl.FragmentPrelude(glsl.GLSLVersionDefault) + "\n"
 
 	tests := []struct {
 		Name    string
@@ -165,7 +165,7 @@ func TestOutput(t *testing.T) {
 			Name:    "Empty",
 			Program: Program{},
 			GlslVS:  ``,
-			GlslFS:  glsl.FragmentPrelude,
+			GlslFS:  glsl.FragmentPrelude(glsl.GLSLVersionDefault),
 		},
 		{
 			Name: "Uniform",
@@ -1019,7 +1019,7 @@ void main(void) {
 	for _, tc := range tests {
 		tc := tc
 		t.Run(tc.Name, func(t *testing.T) {
-			vs, fs := glsl.Compile(&tc.Program)
+			vs, fs := glsl.Compile(&tc.Program, glsl.GLSLVersionDefault)
 			{
 				got := vs
 				want := tc.GlslVS + "\n"
