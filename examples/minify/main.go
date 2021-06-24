a615be69c70c54cdb73dc91e6483927b8d60ba8d
Hajime Hoshi <hajimehoshi@gmail.com>
2021-06-24 21:49:37 +0900

    examples: Add //go:build

--- examples/minify/main.go
+++ examples/minify/main.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build example
 // +build example
 
 // This example is an experiment to minify images with various filters.
--- examples/squiral/main.go
+++ examples/squiral/main.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build example
 // +build example
 
 // This demo is inspired by the xscreensaver 'squirals'.
