80ac0646d5f591562120f0df974c3403e351186c
Hajime Hoshi <hajimehoshi@gmail.com>
2021-07-07 02:07:09 +0900

    internal/graphicsdriver/metal: Optimize the screen writing

--- internal/graphicsdriver/metal/graphics.go
+++ internal/graphicsdriver/metal/graphics.go
@@ -796,10 +796,11 @@ func (g *Graphics) draw(rps mtl.RenderPipelineState, dst *Image, dstRegion drive
 		// (#1019). Always using mtl.LoadActionLoad is safe.
 		if dst.screen {
 			rpd.ColorAttachments[0].LoadAction = mtl.LoadActionClear
+			rpd.ColorAttachments[0].StoreAction = mtl.StoreActionDontCare
 		} else {
 			rpd.ColorAttachments[0].LoadAction = mtl.LoadActionLoad
+			rpd.ColorAttachments[0].StoreAction = mtl.StoreActionStore
 		}
-		rpd.ColorAttachments[0].StoreAction = mtl.StoreActionStore
 
 		t := dst.mtlTexture()
 		g.lastDstTexture = t
