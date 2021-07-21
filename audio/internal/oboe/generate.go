5750c48a3bf8db740d66b4f19d156afa86f70502
Hajime Hoshi <hajimehoshi@gmail.com>
2021-07-21 21:33:31 +0900

    audio/internal/oboe: Remove 'generate' build tag

--- audio/internal/oboe/generate.go
+++ audio/internal/oboe/generate.go
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build generate
-// +build generate
-
 //go:generate go run gen.go
 
 package oboe
