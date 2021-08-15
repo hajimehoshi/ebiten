6bde07f04f64e4b7e03bd8e8255e41fce2f62d7f
Hajime Hoshi <hajimehoshi@gmail.com>
2021-08-16 00:48:17 +0900

    Add Steam SDK tests for x86 (GOARCH=386)

--- .github/workflows/steam.sh
+++ .github/workflows/steam.sh
@@ -3,8 +3,8 @@ export CGO_CFLAGS=-std=gnu99
 export DISPLAY=:99.0
 
 # Install Go
-curl -L --output go${1}.linux-amd64.tar.gz https://golang.org/dl/go${1}.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go${1}.linux-amd64.tar.gz
+curl -L --output ${GO_FILENAME} https://golang.org/dl/${GO_FILENAME}
+rm -rf /usr/local/go && tar -C /usr/local -xzf ${GO_FILENAME}
 
 # Run X
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
