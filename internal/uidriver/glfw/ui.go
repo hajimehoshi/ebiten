0ac927f0047a245a953f3e0fe834012fdb659985
Hajime Hoshi <hajimehoshi@gmail.com>
2021-09-12 23:38:38 +0900

    internal/uidriver/glfw: Bug fix: Forbide Maximize/MinimizeWindow on the native fullscreen on macOS
    Closes #1810

--- internal/uidriver/glfw/ui.go
+++ internal/uidriver/glfw/ui.go
@@ -1383,6 +1383,10 @@ func (u *UserInterface) Window() driver.Window {
 
 // maximizeWindow must be called from the main thread.
 func (u *UserInterface) maximizeWindow() {
+	if u.isNativeFullscreen() {
+		return
+	}
+
 	if u.setSizeCallbackEnabled {
 		u.setSizeCallbackEnabled = false
 		defer func() {
@@ -1408,6 +1412,10 @@ func (u *UserInterface) maximizeWindow() {
 
 // iconifyWindow must be called from the main thread.
 func (u *UserInterface) iconifyWindow() {
+	if u.isNativeFullscreen() {
+		return
+	}
+
 	if u.setSizeCallbackEnabled {
 		u.setSizeCallbackEnabled = false
 		defer func() {
