9154dc06bf8abc4d924d4927e71f17e97573a73e
Zyko <bertrand.jung65@gmail.com>
2021-11-01 20:29:26 +0100

    Dummy uniform uploads on 4 shaders

--- examples/issue_1848/main.go
+++ examples/issue_1848/main.go
@@ -19,11 +19,13 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"math/rand"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/examples/issue_1848/shaders"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
@@ -34,59 +36,118 @@ const (
 )
 
 var (
-	shaderPaletteHorizontal *ebiten.Shader
-	shaderPaletteVertical   *ebiten.Shader
-	shaderPrograms          []*ebiten.Shader
+	shaderSinLayer0            *ebiten.Shader
+	shaderEightCirclesLayer1   *ebiten.Shader
+	shaderDancingX             *ebiten.Shader
+	shaderRandomPostprocessing *ebiten.Shader
+	shaderPrograms             []*ebiten.Shader
 
-	uniformsByShaderIndex = make([]map[string]interface{}, 2)
+	uniformsByShaderIndex = make([]map[string]interface{}, 5)
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
-	shaderPaletteHorizontal, _ = ebiten.NewShader(shaders.ShaderPaletteHorizontalSrc)
-	shaderPaletteVertical, _ = ebiten.NewShader(shaders.ShaderPaletteVerticalSrc)
+
+	shaderSinLayer0, _ = ebiten.NewShader(shaders.SinLayer0Shader)
+	shaderEightCirclesLayer1, _ = ebiten.NewShader(shaders.EightCircleBackgroundShader)
+	shaderDancingX, _ = ebiten.NewShader(shaders.DancingXShader)
+	shaderRandomPostprocessing, _ := ebiten.NewShader(shaders.RandomMaskShader)
 
 	shaderPrograms = []*ebiten.Shader{
-		shaderPaletteHorizontal,
-		shaderPaletteVertical,
+		shaderSinLayer0,
+		shaderEightCirclesLayer1,
+		shaderDancingX,
+		shaderRandomPostprocessing,
 	}
 
 	randomizeUniforms()
 }
 
 func randomizeUniforms() {
+	// Shader 0
 	uniformsByShaderIndex[0] = map[string]interface{}{
-		"Palette0": []float32{
+		"ScreenSize": []float32{float32(screenWidth), float32(screenHeight)},
+		"Palette": []float32{
 			rand.Float32(), rand.Float32(), rand.Float32(), 0.0,
 			rand.Float32(), rand.Float32(), rand.Float32(), 1. / 3.,
 			rand.Float32(), rand.Float32(), rand.Float32(), 2. / 3.,
 			rand.Float32(), rand.Float32(), rand.Float32(), 1.0,
 		},
+		"Frequency": rand.Float32() * 100,
+		"Scale":     0.2 + rand.Float32()*0.3,
+	}
+	// Shader 1
+	positions := make([]float32, 8*2)
+	for i := range positions {
+		positions[i] = rand.Float32() - 0.5
+	}
+	radiuses := make([]float32, 8)
+	for i := range radiuses {
+		radiuses[i] = rand.Float32() * 0.2
+	}
+	palettes := make([][]float32, 8)
+	for i := range palettes {
+		palettes[i] = []float32{
+			rand.Float32(), rand.Float32(), rand.Float32(), 0.0,
+			rand.Float32(), rand.Float32(), rand.Float32(), 1. / 3.,
+			rand.Float32(), rand.Float32(), rand.Float32(), 2. / 3.,
+			rand.Float32(), rand.Float32(), rand.Float32(), 1.0,
+		}
 	}
 	uniformsByShaderIndex[1] = map[string]interface{}{
-		"Palette1": []float32{
-			rand.Float32(), rand.Float32(), rand.Float32(), 0.0,
-			rand.Float32(), rand.Float32(), rand.Float32(), 1. / 3.,
-			rand.Float32(), rand.Float32(), rand.Float32(), 2. / 3.,
-			rand.Float32(), rand.Float32(), rand.Float32(), 1.0,
+		"ScreenSize": []float32{float32(screenWidth), float32(screenHeight)},
+		"Positions":  positions,
+		"Aliasing":   rand.Float32() * 0.01,
+		"Radiuses":   radiuses,
+		"Palette0":   palettes[0],
+		"Palette1":   palettes[1],
+		"Palette2":   palettes[2],
+		"Palette3":   palettes[3],
+		"Palette4":   palettes[4],
+		"Palette5":   palettes[5],
+		"Palette6":   palettes[6],
+		"Palette7":   palettes[7],
+	}
+	// Shader 2
+	uniformsByShaderIndex[2] = map[string]interface{}{
+		"ScreenSize": []float32{float32(screenWidth), float32(screenHeight)},
+		"Width":      0.1 + rand.Float32()*0.1,
+		"Radius":     rand.Float32() * 0.1,
+		"Aliasing":   rand.Float32() * 0.01,
+		"Frequency":  rand.Float32() * 25,
+		"Palette": []float32{
+			rand.Float32() * 0.1, rand.Float32() * 0.1, rand.Float32() * 0.1, 0.0,
+			rand.Float32() * 0.1, rand.Float32() * 0.1, rand.Float32() * 0.1, 1. / 3.,
+			rand.Float32() * 0.1, rand.Float32() * 0.1, rand.Float32() * 0.1, 2. / 3.,
+			rand.Float32() * 0.1, rand.Float32() * 0.1, rand.Float32() * 0.1, 1.0,
 		},
+	}
+	// Shader 3
+	uniformsByShaderIndex[3] = map[string]interface{}{}
+	for i := 0; i < 25; i++ {
+		uniformsByShaderIndex[3][fmt.Sprintf("Maskvar%d", i)] = rand.Float32() * 0.1
 	}
 }
 
 type Game struct {
-	ticks int64
-	idx   int
+	ticks  int64
+	idx    int
+	buffer *ebiten.Image
 }
 
 func (g *Game) Update() error {
+	var switched bool
+
 	g.ticks++
 	if inpututil.IsKeyJustPressed(ebiten.KeyArrowDown) {
 		g.idx++
 		g.idx %= len(shaderPrograms)
+		switched = true
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyArrowUp) {
 		g.idx += len(shaderPrograms) - 1
 		g.idx %= len(shaderPrograms)
+		switched = true
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyR) {
 		randomizeUniforms()
@@ -94,19 +155,57 @@ func (g *Game) Update() error {
 	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
 		return errors.New("soft kill")
 	}
+	if switched {
+		fmt.Println("shader switch")
+	}
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	g.buffer.Clear()
+	// Sin layer 0 background
+	g.buffer.DrawRectShader(
+		screenWidth,
+		screenHeight,
+		shaderPrograms[0],
+		&ebiten.DrawRectShaderOptions{
+			Uniforms: uniformsByShaderIndex[0],
+		},
+	)
+	// 8 circles background
+	g.buffer.DrawRectShader(
+		screenWidth,
+		screenHeight,
+		shaderPrograms[1],
+		&ebiten.DrawRectShaderOptions{
+			Uniforms: uniformsByShaderIndex[1],
+		},
+	)
+	// Dancing X
+	uniformsByShaderIndex[2]["Time"] = float32(g.ticks%240)/240 - 0.5
+	g.buffer.DrawRectShader(
+		screenWidth,
+		screenHeight,
+		shaderPrograms[2],
+		&ebiten.DrawRectShaderOptions{
+			Uniforms: uniformsByShaderIndex[2],
+		},
+	)
+	// Random post processing 1 out of desperation :)
 	screen.DrawRectShader(
 		screenWidth,
 		screenHeight,
-		shaderPrograms[g.idx],
+		shaderPrograms[3],
 		&ebiten.DrawRectShaderOptions{
-			Uniforms: uniformsByShaderIndex[g.idx],
+			Uniforms: uniformsByShaderIndex[3],
+			Images: [4]*ebiten.Image{
+				g.buffer,
+			},
 		},
 	)
+
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f - FPS: %0.2f\n", ebiten.CurrentTPS(), ebiten.CurrentFPS()))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -114,9 +213,14 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
+	ebiten.SetFPSMode(ebiten.FPSModeVsyncOffMaximum)
+	ebiten.SetMaxTPS(60)
+	ebiten.SetFullscreen(true)
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Uniforms uploads test")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(&Game{
+		buffer: ebiten.NewImage(screenWidth, screenHeight),
+	}); err != nil {
 		log.Fatal(err)
 	}
 }
--- examples/issue_1848/shaders/shaders.go
+++ examples/issue_1848/shaders/shaders.go
@@ -1,42 +1,134 @@
 package shaders
 
 var (
-	ShaderPaletteHorizontalSrc = []byte(
-		`
-	package main
-
-	var Palette0 [4]vec4
-	
-	func palette(t float, a, b, c, d vec4) vec3 {
-		var clr vec3
-	
-		if t >= a.a && t <= b.a {
-			clr = mix(a.rgb*a.rgb, b.rgb*b.rgb, vec3((t-a.a)/(b.a-a.a)))
-		} else if t >= b.a && t <= c.a {
-			clr = mix(b.rgb*b.rgb, c.rgb*c.rgb, vec3((t-b.a)/(c.a-b.a)))
-		} else {
-			clr = mix(c.rgb*c.rgb, d.rgb*d.rgb, vec3((t-c.a)/(d.a-c.a)))
-		}
-		clr = clr*clr*(3.0-2.0*clr) // cubic smoothing
-		return sqrt(clr)
-	}
-	
-	func Fragment(position vec4, texCoord vec2, color vec4) vec4 {
-		p := position.xy/imageDstTextureSize()
-		t := abs(length(p*2-vec2(1., 0.5))-0.5)
-		t = p.x
-
-		clr := palette(t, Palette0[0], Palette0[1], Palette0[2], Palette0[3])
-	
-		return vec4(clr, 1.0)
-	}
-`)
-
-	ShaderPaletteVerticalSrc = []byte(
+	SinLayer0Shader = []byte(
 		`
 package main
 
+var ScreenSize vec2
+var Palette [4]vec4
+var Frequency float
+var Scale float
+	
+func palette(t float, a, b, c, d vec4) vec3 {
+	var clr vec3
+
+	t = abs(t)
+	if t >= a.a && t <= b.a {
+		clr = mix(a.rgb*a.rgb, b.rgb*b.rgb, vec3((t-a.a)/(b.a-a.a)))
+	} else if t >= b.a && t <= c.a {
+		clr = mix(b.rgb*b.rgb, c.rgb*c.rgb, vec3((t-b.a)/(c.a-b.a)))
+	} else {
+		clr = mix(c.rgb*c.rgb, d.rgb*d.rgb, vec3((t-c.a)/(d.a-c.a)))
+	}
+	clr = clr*clr*(3.0-2.0*clr)
+	return sqrt(clr)
+}
+
+func Fragment(position vec4, texCoord vec2, color vec4) vec4 {
+	// Convert position in [-1, 1]
+	p := position.xy / ScreenSize
+	p -= 0.5
+	p /= Scale
+
+	c0 := palette(p.x, Palette[0], Palette[1], Palette[2], Palette[3])
+	c1 := palette(p.y, Palette[0], Palette[1], Palette[2], Palette[3])
+	t := sin(p.x*Frequency) + sin(p.y*Frequency)
+
+	return vec4(mix(c0, c1, t), 1)
+}
+`)
+
+	EightCircleBackgroundShader = []byte(
+		`
+package main
+
+var ScreenSize vec2
+var Positions [8]vec2
+var Radiuses [8]float
+var Aliasing float
+
+var Palette0 [4]vec4
 var Palette1 [4]vec4
+var Palette2 [4]vec4
+var Palette3 [4]vec4
+var Palette4 [4]vec4
+var Palette5 [4]vec4
+var Palette6 [4]vec4
+var Palette7 [4]vec4
+	
+func palette(t float, a, b, c, d vec4) vec3 {
+	var clr vec3
+
+	t = -t
+	if t >= a.a && t <= b.a {
+		clr = mix(a.rgb*a.rgb, b.rgb*b.rgb, vec3((t-a.a)/(b.a-a.a)))
+	} else if t >= b.a && t <= c.a {
+		clr = mix(b.rgb*b.rgb, c.rgb*c.rgb, vec3((t-b.a)/(c.a-b.a)))
+	} else {
+		clr = mix(c.rgb*c.rgb, d.rgb*d.rgb, vec3((t-c.a)/(d.a-c.a)))
+	}
+	clr = clr*clr*(3.0-2.0*clr)
+	return sqrt(clr)
+}
+
+func Fragment(position vec4, texCoord vec2, color vec4) vec4 {
+	p := position.xy / ScreenSize.xy
+	p -= 0.5
+
+	// Drawing circles
+	idx := -1
+	shortestDistance := 0.
+	for i := 0; i < 8; i++ {
+		r := Radiuses[i]
+		pos := p - Positions[i]
+		d := length(pos) - r
+		if d < shortestDistance {
+			idx = i
+			shortestDistance = d
+		}
+	}
+	// We're not in a circle
+	if idx < 0 {
+		return vec4(0)
+	}
+
+	// Resolving color
+	var c vec4
+	if idx == 0 {
+		c = vec4(palette(shortestDistance, Palette0[0], Palette0[1], Palette0[2], Palette0[3]), 1)
+	} else if idx == 1 {
+		c = vec4(palette(shortestDistance, Palette1[0], Palette1[1], Palette1[2], Palette1[3]), 1)
+	} else if idx == 2 {
+		c = vec4(palette(shortestDistance, Palette2[0], Palette2[1], Palette2[2], Palette2[3]), 1)
+	} else if idx == 3 {
+		c = vec4(palette(shortestDistance, Palette3[0], Palette3[1], Palette3[2], Palette3[3]), 1)
+	} else if idx == 4 {
+		c = vec4(palette(shortestDistance, Palette4[0], Palette4[1], Palette4[2], Palette4[3]), 1)
+	} else if idx == 5 {
+		c = vec4(palette(shortestDistance, Palette5[0], Palette5[1], Palette5[2], Palette5[3]), 1)
+	} else if idx == 6 {
+		c = vec4(palette(shortestDistance, Palette6[0], Palette6[1], Palette6[2], Palette6[3]), 1)
+	} else if idx == 7 {
+		c = vec4(palette(shortestDistance, Palette7[0], Palette7[1], Palette7[2], Palette7[3]), 1)
+	}
+
+	return c*(1-smoothstep(Aliasing, 0., -shortestDistance))
+}
+`)
+
+	DancingXShader = []byte(
+		`
+package main
+
+var ScreenSize vec2
+var Width float
+var Radius float
+var Aliasing float
+var Frequency float
+var Palette [4]vec4
+
+var Time float
 
 func palette(t float, a, b, c, d vec4) vec3 {
 	var clr vec3
@@ -48,18 +140,85 @@ func palette(t float, a, b, c, d vec4) vec3 {
 	} else {
 		clr = mix(c.rgb*c.rgb, d.rgb*d.rgb, vec3((t-c.a)/(d.a-c.a)))
 	}
-	clr = clr*clr*(3.0-2.0*clr) // cubic smoothing
+	clr = clr*clr*(3.0-2.0*clr)
 	return sqrt(clr)
 }
 
 func Fragment(position vec4, texCoord vec2, color vec4) vec4 {
-	p := position.xy/imageDstTextureSize()
-	t := abs(length(p*2-vec2(1., 0.5))-0.5)
-	t = p.y
+	p := position.xy / ScreenSize.xy
+	p -= 0.5
+	p -= vec2(Time, sin(Time*Frequency)/3)
+	p = abs(p)
+    d := length(p-min(p.x+p.y,Width)*0.5) - Radius
+	if d > 0. {
+		return vec4(0)
+	}
 
-	clr := palette(t, Palette1[0], Palette1[1], Palette1[2], Palette1[3])
+	c := vec4(palette(d, Palette[0], Palette[1], Palette[2], Palette[3]), 1)
+	return c*(1-smoothstep(Aliasing, 0., -d))
+}
+`)
 
-	return vec4(clr, 1.0)
+	RandomMaskShader = []byte(
+		`
+package main
+
+// 25 useless uniforms of because desperate
+var Maskvar0 float
+var Maskvar1 float
+var Maskvar2 float
+var Maskvar3 float
+var Maskvar4 float
+var Maskvar5 float
+var Maskvar6 float
+var Maskvar7 float
+var Maskvar8 float
+var Maskvar9 float
+var Maskvar10 float
+var Maskvar11 float
+var Maskvar12 float
+var Maskvar13 float
+var Maskvar14 float
+var Maskvar15 float
+var Maskvar16 float
+var Maskvar17 float
+var Maskvar18 float
+var Maskvar19 float
+var Maskvar20 float
+var Maskvar21 float
+var Maskvar22 float
+var Maskvar23 float
+var Maskvar24 float
+
+func Fragment(position vec4, texCoord vec2, color vec4) vec4 {
+	mask := vec4(1, 0, 0, 1)
+	mask *= (Maskvar0*0.01)
+	mask *= (Maskvar1*0.01)
+	mask *= (Maskvar2*0.01)
+	mask *= (Maskvar3*0.01)
+	mask *= (Maskvar4*0.01)	
+	mask *= (Maskvar5*0.01)
+	mask *= (Maskvar6*0.01)
+	mask *= (Maskvar7*0.01)
+	mask *= (Maskvar8*0.01)
+	mask *= (Maskvar9*0.01)
+	mask *= (Maskvar10*0.01)
+	mask *= (Maskvar11*0.01)
+	mask *= (Maskvar12*0.01)
+	mask *= (Maskvar13*0.01)
+	mask *= (Maskvar14*0.01)
+	mask *= (Maskvar15*0.01)
+	mask *= (Maskvar16*0.01)
+	mask *= (Maskvar17*0.01)
+	mask *= (Maskvar18*0.01)
+	mask *= (Maskvar19*0.01)
+	mask *= (Maskvar20*0.01)
+	mask *= (Maskvar21*0.01)
+	mask *= (Maskvar22*0.01)
+	mask *= (Maskvar23*0.01)
+	mask *= (Maskvar24*0.01)
+
+	return imageSrc0UnsafeAt(texCoord)+mask
 }
 `)
 )
