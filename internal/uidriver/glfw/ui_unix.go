bdf78dab27f545848ef2199de132028f01e1cf98
Hajime Hoshi <hajimehoshi@gmail.com>
2021-11-27 18:52:33 +0900

    internal/uidriver/glfw: Clean up build tags

--- internal/uidriver/glfw/ui_unix.go
+++ internal/uidriver/glfw/ui_unix.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !android && !darwin && !js && !ios && !windows
-// +build !android,!darwin,!js,!ios,!windows
+//go:build !android && !darwin && !js && !windows
+// +build !android,!darwin,!js,!windows
 
 package glfw
 
