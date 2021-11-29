b81736793d6792a9089f16663f3d880a5d9bc7e9
Hajime Hoshi <hajimehoshi@gmail.com>
2021-11-29 19:36:28 +0900

    Revert "audio: Refactoring: Use the underlying source's Seek in Read"
    This reverts commit 15ba3ed4d1265be5e723d53084960c76195e85d1.
    
    Reason: Test failure

--- audio/loop.go
+++ audio/loop.go
@@ -83,13 +83,12 @@ func (i *InfiniteLoop) Read(b []byte) (int, error) {
 	}
 
 	if err == io.EOF || i.pos == i.length() {
-		pos, err := i.src.Seek(i.lstart, io.SeekStart)
+		pos, err := i.Seek(i.lstart, io.SeekStart)
 		if err != nil {
 			return 0, err
 		}
 		i.pos = pos
 	}
-
 	return n, nil
 }
 
