40c7a4c85634ff062b3153d07e0050e4121988c4
Changkun Ou <hi@changkun.de>
2022-01-12 08:25:14 +0100

    internal/graphicscommand: pending calls if not on the mainthread

--- internal/graphicscommand/thread.go
+++ internal/graphicscommand/thread.go
@@ -14,7 +14,32 @@
 
 package graphicscommand
 
-var theThread Thread
+import (
+	"runtime"
+	"sync"
+)
+
+var (
+	theThread    Thread
+	theThreadId  uint64
+	mainthreadId uint64
+
+	mu      sync.Mutex
+	pending []func()
+)
+
+func init() {
+	mainthreadId = gid()
+}
+
+func gid() (id uint64) {
+	var buf [30]byte
+	runtime.Stack(buf[:], false)
+	for i := 10; buf[i] != ' '; i++ {
+		id = id*10 + uint64(buf[i]&15)
+	}
+	return id
+}
 
 type Thread interface {
 	Call(f func())
@@ -23,16 +48,35 @@ type Thread interface {
 // SetMainThread must be called from the main thread (i.e, the goroutine where the thread is created).
 func SetMainThread(thread Thread) {
 	theThread = thread
+	theThreadId = gid()
 }
 
 // RunOnMainThread calls f on the main thread, and returns an error if any.
 func RunOnMainThread(f func()) {
+	if theThreadId != mainthreadId {
+		mu.Lock()
+		pending = append(pending, f)
+		mu.Unlock()
+		return
+	}
+
 	// The thread is nil when 1) GOOS=js or 2) using golang.org/x/mobile/gl.
 	// When golang.org/x/mobile/gl is used, all the GL functions are called via Context, which already runs on an
 	// appropriate thread.
+	var caller func(func())
 	if theThread == nil {
-		f()
-		return
+		caller = func(f func()) { f() }
+	} else {
+		caller = theThread.Call
 	}
-	theThread.Call(f)
+
+	mu.Lock()
+	if len(pending) > 0 {
+		for i := range pending {
+			caller(pending[i])
+		}
+		pending = []func(){}
+	}
+	mu.Unlock()
+	caller(f)
 }
