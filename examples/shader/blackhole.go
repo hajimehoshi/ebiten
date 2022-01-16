baf1c2b575ab52a09b3aae756538eeec25351f68
F. Talha Altinel <talhaaltinel@hotmail.com>
2022-01-16 02:59:37 +0000

    applied PR feedbacks

--- examples/shader/blackhole.go
+++ examples/shader/blackhole.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The Ebiten Authors
+// Copyright 2022 The Ebiten Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,16 +22,16 @@ var ScreenSize vec2
 var Cursor vec2
 
 func Fragment(position vec4, texCoord vec2, color vec4) vec4 {
-	uv := position.xy / ScreenSize.xy
-	texC := uv
+	origin, size := imageDstRegionOnTexture()
+	texC := (position.xy/imageDstTextureSize() - origin) / size
+	texC2 := (position.xy/imageDstTextureSize().x - origin) / size.x
 	lpos := Cursor.xy / ScreenSize.x
-	texC2 := position.xy / ScreenSize.x
-	texC = mix(texC2, (texC*2.0-lpos*2.0)*0.2*0.5+lpos, (1.0 / (distance((texC2*2.0-lpos*2.0)*10.0*0.5+lpos, lpos) - 1.0)))
+	texC = mix(texC2, (texC*2.0-lpos*2.0)*0.4*0.5+lpos, (1.0 / (distance((texC2*2.0-lpos*2.0)*10*0.5+lpos, lpos) - 1.0)))
 
 	ratio := ScreenSize.x / ScreenSize.y
 	texC.y *= ratio
-	origin, size := imageSrcRegionOnTexture()
-	texC = texC*size + origin
+	origin1, size1 := imageSrcRegionOnTexture()
+	texC = texC*size1 + origin1
 
 	resColor := imageSrc2At(texC).rgb
 	resColor *= clamp(pow(distance(texC2, lpos), 8.8)*300000000.0, 0.0, 1.0)
