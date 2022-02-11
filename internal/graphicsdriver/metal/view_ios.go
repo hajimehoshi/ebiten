cc4e2f08be291edcc9a9587e8a03e7e907f023ac
Hajime Hoshi <hajimehoshi@gmail.com>
2022-02-11 21:46:11 +0900

    internal/graphicsdriver/metal: rename files

--- internal/graphicsdriver/metal/view_ios.go
+++ internal/graphicsdriver/metal/view_ios.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build darwin && ios
-// +build darwin,ios
+//go:build ios
+// +build ios
 
 package metal
 
