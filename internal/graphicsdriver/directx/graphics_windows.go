9d9439751f1a0e9c987a409687ac2ba57f6b5626
Hajime Hoshi <hajimehoshi@gmail.com>
2022-04-26 01:12:52 +0900

    internal/graphicsdriver/directx: bug fix: the screen image can be nil when resizing the window
    Closes #2081

--- internal/graphicsdriver/directx/graphics_windows.go
+++ internal/graphicsdriver/directx/graphics_windows.go
@@ -577,7 +577,8 @@ func (g *Graphics) End(present bool) error {
 		return err
 	}
 
-	if present {
+	// The screen image can be nil when resizing the window (#2081).
+	if present && g.screenImage != nil {
 		g.screenImage.transiteState(g.drawCommandList, _D3D12_RESOURCE_STATE_PRESENT)
 	}
 
