e5b3aa6177195ae236cd2da903a66f2d2949b4a1
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-05-20 06:31:18 -0400

    internal/opengl/gl: add comment to link to OpenGL framework

--- internal/graphicsdriver/opengl/gl/procaddr_darwin.go
+++ internal/graphicsdriver/opengl/gl/procaddr_darwin.go
@@ -6,6 +6,9 @@ import (
 	"github.com/ebiten/purego"
 )
 
+// this comment tells the linker to link to the OpenGL framework at runtime
+//go:cgo_import_dynamic _ _ "/System/Library/Frameworks/OpenGL.framework/Versions/Current/OpenGL"
+
 func getProcAddress(name string) uintptr {
 	return purego.Dlsym(purego.RTLD_DEFAULT, name)
 }
