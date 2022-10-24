c5c52e50c9d1742194c3150c0fa859d2863e9fa9
Skenvy <>
2022-10-24 23:45:46 +1100

    Move the package gles comment from const to interface and remove const from gl package

--- internal/graphicsdriver/opengl/gl/package.go
+++ internal/graphicsdriver/opengl/gl/package.go
@@ -15,56 +15,6 @@
 // Package gl implements Go bindings to OpenGL.
 package gl
 
-const (
-	ALWAYS               = 0x0207
-	ARRAY_BUFFER         = 0x8892
-	BLEND                = 0x0BE2
-	CLAMP_TO_EDGE        = 0x812F
-	COLOR_ATTACHMENT0    = 0x8CE0
-	COMPILE_STATUS       = 0x8B81
-	DEPTH24_STENCIL8     = 0x88F0
-	DYNAMIC_DRAW         = 0x88E8
-	ELEMENT_ARRAY_BUFFER = 0x8893
-	FALSE                = 0
-	FLOAT                = 0x1406
-	FRAGMENT_SHADER      = 0x8B30
-	FRAMEBUFFER          = 0x8D40
-	FRAMEBUFFER_BINDING  = 0x8CA6
-	FRAMEBUFFER_COMPLETE = 0x8CD5
-	INFO_LOG_LENGTH      = 0x8B84
-	INVERT               = 0x150A
-	KEEP                 = 0x1E00
-	LINK_STATUS          = 0x8B82
-	MAX_TEXTURE_SIZE     = 0x0D33
-	NEAREST              = 0x2600
-	NO_ERROR             = 0
-	NOTEQUAL             = 0x0205
-	PIXEL_PACK_BUFFER    = 0x88EB
-	PIXEL_UNPACK_BUFFER  = 0x88EC
-	READ_WRITE           = 0x88BA
-	RENDERBUFFER         = 0x8D41
-	RGBA                 = 0x1908
-	SHORT                = 0x1402
-	STENCIL_ATTACHMENT   = 0x8D20
-	STENCIL_BUFFER_BIT   = 0x0400
-	STENCIL_TEST         = 0x0B90
-	STREAM_DRAW          = 0x88E0
-	TEXTURE0             = 0x84C0
-	TEXTURE_2D           = 0x0DE1
-	TEXTURE_MAG_FILTER   = 0x2800
-	TEXTURE_MIN_FILTER   = 0x2801
-	TEXTURE_WRAP_S       = 0x2802
-	TEXTURE_WRAP_T       = 0x2803
-	TRIANGLES            = 0x0004
-	TRUE                 = 1
-	SCISSOR_TEST         = 0x0C11
-	UNPACK_ALIGNMENT     = 0x0CF5
-	UNSIGNED_BYTE        = 0x1401
-	UNSIGNED_SHORT       = 0x1403
-	VERTEX_SHADER        = 0x8B31
-	WRITE_ONLY           = 0x88B9
-)
-
 // Init initializes the OpenGL bindings by loading the function pointers (for
 // each OpenGL function) from the active OpenGL context.
 //
--- internal/graphicsdriver/opengl/gles/interface.go
+++ internal/graphicsdriver/opengl/gles/interface.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package gles implements Go bindings to OpenGL ES.
 package gles
 
 type Context interface {
