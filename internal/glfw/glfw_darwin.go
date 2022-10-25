d4da1c64bdfa14a04ccd8f6610d753ca52665fa7
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-10-24 23:12:00 -0400

    reorder imports

--- internal/glfw/glfw_darwin.go
+++ internal/glfw/glfw_darwin.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Ebiten Authors
+// Copyright 2022 The Ebiten Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
 package glfw
 
 import (
-	"github.com/ebitengine/purego"
 	"image"
 	"image/draw"
 	"math"
@@ -24,6 +23,8 @@ import (
 	"runtime"
 	"sync"
 	"unsafe"
+
+	"github.com/ebitengine/purego"
 )
 
 type glfwImage struct {
