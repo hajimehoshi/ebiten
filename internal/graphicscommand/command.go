5eceabd1720eac9680b46cd8dfa210ee664ee114
Hajime Hoshi <hajimehoshi@gmail.com>
2022-10-30 02:32:09 +0900

    Revert "internal/graphicscommand: bug fix: flush image buffers"
    This reverts commit d6910eb9aba3cbd7a9d7267da1e386d2c319cbdf.
    
    Reason: The wasm flakiness occured again
    
    Updates #2391
    Updates #2423

--- internal/graphicscommand/command.go
+++ internal/graphicscommand/command.go
@@ -250,7 +250,13 @@ func (q *commandQueue) flush(graphicsDriver graphicsdriver.Graphics, endFrame bo
 // FlushCommands flushes the command queue and present the screen if needed.
 // If endFrame is true, the current screen might be used to present.
 func FlushCommands(graphicsDriver graphicsdriver.Graphics, endFrame bool) error {
-	flushImageBuffers()
+	// Flush image buffers only when the frame ends.
+	// Flushing them in tests might cause test flakiness on browsers (#2391).
+	// TODO: Investigate why.
+	if endFrame {
+		flushImageBuffers()
+	}
+
 	return theCommandQueue.Flush(graphicsDriver, endFrame)
 }
 
