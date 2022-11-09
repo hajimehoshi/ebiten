097adcf8b647b5fc38ae6002ecf878d27ac7caf4
Hajime Hoshi <hajimehoshi@gmail.com>
2022-11-09 16:07:30 +0900

    internal/graphicsdriver/opengl/gl: reladn: refactoring
    This is a reland of a1ad87a2622fbe654f0d1776407def03fd2f91ec

--- internal/graphicsdriver/opengl/gl/conversions_cgo.go
+++ internal/graphicsdriver/opengl/gl/conversions_cgo.go
@@ -17,27 +17,14 @@ func GoStr(cstr *byte) string {
 	return C.GoString((*C.char)(unsafe.Pointer(cstr)))
 }
 
-// Strs takes a list of Go strings (with or without null-termination) and
-// returns their C counterpart.
+// CStr takes a Go string (with or without null-termination)
+// and returns the C counterpart.
 //
-// The returned free function must be called once you are done using the strings
+// The returned free function must be called once you are done using the string
 // in order to free the memory.
-//
-// If no strings are provided as a parameter this function will panic.
-func Strs(strs ...string) (cstrs **byte, free func()) {
-	if len(strs) == 0 {
-		panic("gl: expected at least 1 string at Strs")
-	}
-
-	css := make([]*byte, 0, len(strs))
-	for _, str := range strs {
-		cs := C.CString(str)
-		css = append(css, (*byte)(unsafe.Pointer(cs)))
-	}
-
-	return (**byte)(&css[0]), func() {
-		for _, cs := range css {
-			C.free(unsafe.Pointer(cs))
-		}
+func CStr(str string) (cstr *byte, free func()) {
+	cs := C.CString(str)
+	return (*byte)(unsafe.Pointer(cs)), func() {
+		C.free(unsafe.Pointer(cs))
 	}
 }
