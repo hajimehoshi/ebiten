bb30b85d29d8a1a45859a3c961b93fcfa4b758d1
Hajime Hoshi <hajimehoshi@gmail.com>
2022-11-13 03:35:40 +0900

    internal/graphicsdriver/opengl/gl: simplify C code

--- internal/graphicsdriver/opengl/gl/package_notpurego.go
+++ internal/graphicsdriver/opengl/gl/package_notpurego.go
@@ -18,74 +18,18 @@ package gl
 //   #define GLAPI extern
 // #endif
 //
-// #include <stddef.h>
-//
-// #ifndef GLEXT_64_TYPES_DEFINED
-//   /* This code block is duplicated in glxext.h, so must be protected */
-//   #define GLEXT_64_TYPES_DEFINED
-//   /* Define int32_t, int64_t, and uint64_t types for UST/MSC */
-//   /* (as used in the GL_EXT_timer_query extension). */
-//   #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
-//     #include <inttypes.h>
-//   #elif defined(__sun__) || defined(__digital__)
-//     #include <inttypes.h>
-//     #if defined(__STDC__)
-//       #if defined(__arch64__) || defined(_LP64)
-//         typedef long int int64_t;
-//         typedef unsigned long int uint64_t;
-//       #else
-//         typedef long long int int64_t;
-//         typedef unsigned long long int uint64_t;
-//       #endif /* __arch64__ */
-//     #endif /* __STDC__ */
-//   #elif defined( __VMS ) || defined(__sgi)
-//     #include <inttypes.h>
-//   #elif defined(__SCO__) || defined(__USLC__)
-//     #include <stdint.h>
-//   #elif defined(__UNIXOS2__) || defined(__SOL64__)
-//     typedef long int int32_t;
-//     typedef long long int int64_t;
-//     typedef unsigned long long int uint64_t;
-//   #else
-//     /* Fallback if nothing above works */
-//     #include <inttypes.h>
-//   #endif
-// #endif
+// #include <stdint.h>
 //
 // typedef unsigned int GLenum;
 // typedef unsigned char GLboolean;
 // typedef unsigned int GLbitfield;
-// typedef signed char GLbyte;
-// typedef short GLshort;
 // typedef int GLint;
-// typedef int GLclampx;
-// typedef unsigned char GLubyte;
-// typedef unsigned short GLushort;
 // typedef unsigned int GLuint;
 // typedef int GLsizei;
 // typedef float GLfloat;
-// typedef float GLclampf;
-// typedef double GLdouble;
-// typedef double GLclampd;
-// typedef void *GLeglClientBufferEXT;
-// typedef void *GLeglImageOES;
 // typedef char GLchar;
-// typedef char GLcharARB;
-// #ifdef __APPLE__
-// typedef void *GLhandleARB;
-// #else
-// typedef unsigned int GLhandleARB;
-// #endif
-// typedef GLint GLfixed;
 // typedef ptrdiff_t GLintptr;
 // typedef ptrdiff_t GLsizeiptr;
-// typedef int64_t GLint64;
-// typedef uint64_t GLuint64;
-// typedef ptrdiff_t GLintptrARB;
-// typedef ptrdiff_t GLsizeiptrARB;
-// typedef int64_t GLint64EXT;
-// typedef uint64_t GLuint64EXT;
-// typedef uintptr_t GLsync;
 //
 // typedef void (APIENTRY *GLDEBUGPROC)(GLenum source,GLenum type,GLuint id,GLenum severity,GLsizei length,const GLchar *message,const void *userParam);
 // typedef void (APIENTRY *GLDEBUGPROCARB)(GLenum source,GLenum type,GLuint id,GLenum severity,GLsizei length,const GLchar *message,const void *userParam);
