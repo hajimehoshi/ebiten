e5e7ceb688e7b7674efa4d55344a45394c35ad4f
Hajime Hoshi <hajimehoshi@gmail.com>
2022-11-13 23:10:20 +0900

    internal/graphicsdriver/opengl/gl: refactoring

--- internal/graphicsdriver/opengl/gl/procaddr_others.go
+++ internal/graphicsdriver/opengl/gl/procaddr_others.go
@@ -1,29 +1,18 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2014 Eric Woroshow
 
-//go:build !android && !darwin && !js && !windows && !opengles
+//go:build !android && !darwin && !js && !nintendosdk && !windows && !opengles
 
 package gl
 
-/*
-#cgo !nintendosdk pkg-config: gl
-#cgo nintendosdk  CFLAGS: -DTAG_NINTENDOSDK
-#cgo nintendosdk  LDFLAGS: -Wl,-unresolved-symbols=ignore-all
-
-#if defined(TAG_NINTENDOSDK)
-	#include <stdlib.h>
-	#include <EGL/egl.h>
-	static void* getProcAddress(const char* name) {
-		return eglGetProcAddress(name);
-	}
-#else
-	#include <stdlib.h>
-	#include <GL/glx.h>
-	static void* getProcAddress(const char* name) {
-		return glXGetProcAddress((const GLubyte *) name);
-	}
-#endif
-*/
+// #cgo pkg-config: gl
+//
+// #include <stdlib.h>
+// #include <GL/glx.h>
+//
+// static void* getProcAddress(const char* name) {
+//   return glXGetProcAddress((const GLubyte *) name);
+// }
 import "C"
 
 import "unsafe"
