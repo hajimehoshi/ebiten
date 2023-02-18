f68601f6c2c9fd731e300fbc0de37e2925971a12
nitinmewar <nitinmewar28@gmail.com>
2023-02-18 07:00:12 +0530

    debug
    Signed-off-by: nitinmewar <nitinmewar28@gmail.com>

--- .github/workflows/get_go_version.sh
+++ .github/workflows/get_go_version.sh
@@ -3,8 +3,9 @@
 # Retrieve the JSON data from the website
 json=$(curl -s https://go.dev/dl/?mode=json)
 
-# Extract the Go versions from the JSON data
-versions=$(echo "$json" | jq -r '.[].version')
+# Extract Go versions from JSON data
+versions=$(echo "$json" | jq -r '.[].version' | sed 's/^go//')
+echo "export GO_VERSIONS=\"$versions\"" >> ~/.bashrc
 
-# Set an environment variable with the Go versions
+# set environment variable
 export GO_VERSIONS="$versions"
