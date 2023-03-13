b1048025d90e9e0e7ebbd01ac7cf562cb07a3ddd
Hajime Hoshi <hajimehoshi@gmail.com>
2023-03-14 00:45:10 +0900

    ebiten: separate graphics.go into compositemode.go and it

--- graphics.go
+++ graphics.go
@@ -15,8 +15,6 @@
 package ebiten
 
 import (
-	"fmt"
-
 	"github.com/hajimehoshi/ebiten/v2/internal/builtinshader"
 	"github.com/hajimehoshi/ebiten/v2/internal/ui"
 )
@@ -31,100 +29,6 @@ const (
 	// FilterLinear represents linear filter
 	FilterLinear Filter = Filter(builtinshader.FilterLinear)
 )
-
-// CompositeMode represents Porter-Duff composition mode.
-//
-// Deprecated: as of v2.5. Use Blend instead.
-type CompositeMode int
-
-const (
-	// CompositeModeCustom indicates to refer Blend.
-	CompositeModeCustom CompositeMode = iota
-
-	// Deprecated: as of v2.5. Use BlendSourceOver instead.
-	CompositeModeSourceOver
-
-	// Deprecated: as of v2.5. Use BlendClear instead.
-	CompositeModeClear
-
-	// Deprecated: as of v2.5. Use BlendCopy instead.
-	CompositeModeCopy
-
-	// Deprecated: as of v2.5. Use BlendDestination instead.
-	CompositeModeDestination
-
-	// Deprecated: as of v2.5. Use BlendDestinationOver instead.
-	CompositeModeDestinationOver
-
-	// Deprecated: as of v2.5. Use BlendSourceIn instead.
-	CompositeModeSourceIn
-
-	// Deprecated: as of v2.5. Use BlendDestinationIn instead.
-	CompositeModeDestinationIn
-
-	// Deprecated: as of v2.5. Use BlendSourceOut instead.
-	CompositeModeSourceOut
-
-	// Deprecated: as of v2.5. Use BlendDestinationOut instead.
-	CompositeModeDestinationOut
-
-	// Deprecated: as of v2.5. Use BlendSourceAtop instead.
-	CompositeModeSourceAtop
-
-	// Deprecated: as of v2.5. Use BlendDestinationAtop instead.
-	CompositeModeDestinationAtop
-
-	// Deprecated: as of v2.5. Use BlendXor instead.
-	CompositeModeXor
-
-	// Deprecated: as of v2.5. Use BlendLighter instead.
-	CompositeModeLighter
-
-	// Deprecated: as of v2.5. Use Blend with BlendFactorDestinationColor and BlendFactorZero instead.
-	CompositeModeMultiply
-)
-
-func (c CompositeMode) blend() Blend {
-	switch c {
-	case CompositeModeSourceOver:
-		return BlendSourceOver
-	case CompositeModeClear:
-		return BlendClear
-	case CompositeModeCopy:
-		return BlendCopy
-	case CompositeModeDestination:
-		return BlendDestination
-	case CompositeModeDestinationOver:
-		return BlendDestinationOver
-	case CompositeModeSourceIn:
-		return BlendSourceIn
-	case CompositeModeDestinationIn:
-		return BlendDestinationIn
-	case CompositeModeSourceOut:
-		return BlendSourceOut
-	case CompositeModeDestinationOut:
-		return BlendDestinationOut
-	case CompositeModeSourceAtop:
-		return BlendSourceAtop
-	case CompositeModeDestinationAtop:
-		return BlendDestinationAtop
-	case CompositeModeXor:
-		return BlendXor
-	case CompositeModeLighter:
-		return BlendLighter
-	case CompositeModeMultiply:
-		return Blend{
-			BlendFactorSourceRGB:        BlendFactorDestinationColor,
-			BlendFactorSourceAlpha:      BlendFactorDestinationColor,
-			BlendFactorDestinationRGB:   BlendFactorZero,
-			BlendFactorDestinationAlpha: BlendFactorZero,
-			BlendOperationRGB:           BlendOperationAdd,
-			BlendOperationAlpha:         BlendOperationAdd,
-		}
-	default:
-		panic(fmt.Sprintf("ebiten: invalid composite mode: %d", c))
-	}
-}
 
 // GraphicsLibrary represents graphics libraries supported by the engine.
 type GraphicsLibrary int
