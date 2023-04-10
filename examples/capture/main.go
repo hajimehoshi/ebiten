8c4bdc0515f3980ea63515a9d483090aa9ea32cf
Eric H <harbdog@users.noreply.github.com>
2023-04-10 09:10:04 -0400

    corrected name as ebitengine

--- examples/capture/main.go
+++ examples/capture/main.go
@@ -1,4 +1,4 @@
-// Copyright 2023 The Ebiten Authors
+// Copyright 2023 The Ebitengine Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
