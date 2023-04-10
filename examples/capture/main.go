0467038d1cf9dd863e473b2180238cf7ac842de0
Eric H <harbdog@users.noreply.github.com>
2023-04-09 22:18:29 -0400

    update copied copyright

--- examples/capture/main.go
+++ examples/capture/main.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Ebiten Authors
+// Copyright 2023 The Ebiten Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
