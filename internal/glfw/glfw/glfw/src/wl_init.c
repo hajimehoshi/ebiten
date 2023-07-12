8ee3a6d0c1542dc2db59707e1cc09ddeee498cb0
Hajime Hoshi <hajimehoshi@gmail.com>
2023-07-13 02:32:32 +0900

    internal/glfw/glfw/glfw/src: remove warnings for Wayland

--- internal/glfw/glfw/glfw/src/wl_init.c
+++ internal/glfw/glfw/glfw/src/wl_init.c
@@ -26,6 +26,10 @@
 // It is fine to use C99 in this file because it will not be built with VS
 //========================================================================
 
+#if !defined(_GNU_SOURCE)
+ #define _GNU_SOURCE
+#endif
+
 #define _POSIX_C_SOURCE 200809L
 
 #include "internal.h"
