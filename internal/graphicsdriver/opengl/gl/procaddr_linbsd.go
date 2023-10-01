b2a6e79a5a0b5ae2fc3f9b599959e3edad01e63a
Hajime Hoshi <hajimehoshi@gmail.com>
2023-10-02 01:02:51 +0900

    internal/graphicsdriver/opengl/gl: rename procaddr_others.go to procaddr_linbsd.go

--- internal/graphicsdriver/opengl/gl/procaddr_linbsd.go
+++ internal/graphicsdriver/opengl/gl/procaddr_linbsd.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !darwin && !js && !nintendosdk && !windows
+//go:build (freebsd || linux || netbsd || openbsd) && !nintendosdk
 
 package gl
 
