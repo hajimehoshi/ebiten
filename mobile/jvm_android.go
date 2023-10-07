e352181ec14d04daa8a3e0a73156bf07b93e9054
Hajime Hoshi <hajimehoshi@gmail.com>
2023-10-08 02:14:57 +0900

    mobile: add RunOnJVM
    RunOnJVM is just a wrapper of golang.org/x/mobile/app.RunOnJVM,
    in order to reduce direct dependencies on golang.org/x/mobile.
    
    Updates #2803

---
+++ mobile/jvm_android.go
@@ -0,0 +1,34 @@
+// Copyright 2023 The Ebitengine Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mobile
+
+import (
+	"golang.org/x/mobile/app"
+)
+
+// RunOnJVM runs fn on a new goroutine locked to an OS thread with a JNIEnv.
+//
+// RunOnJVM is a wrapper of golang.org/x/mobile/app's RunOnJVM.
+// RunOnJVM is preferable in order to reduce a direct dependency on golang.org/x/mobile in your application.
+//
+// RunOnJVM blocks until the call to fn is complete. Any Java
+// exception or failure to attach to the JVM is returned as an error.
+//
+// The function fn takes vm, the current JavaVM*,
+// env, the current JNIEnv*, and
+// ctx, a jobject representing the global android.context.Context.
+func RunOnJVM(fn func(vm, jniEnv, ctx uintptr) error) error {
+	return app.RunOnJVM(fn)
+}
