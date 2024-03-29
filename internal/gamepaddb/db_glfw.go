24238e16af65be8f5813fda2fa4f1c6a5e62d905
Hajime Hoshi <hajimehoshi@gmail.com>
2024-03-29 13:18:13 +0900

    internal/gamepaddb: refactoring
    Updates #2936

--- internal/gamepaddb/db_glfw.go
+++ internal/gamepaddb/db_glfw.go
@@ -16,10 +16,6 @@
 
 package gamepaddb
 
-import (
-	_ "embed"
-)
-
 var additionalGLFWGamepads = []byte(`
 78696e70757401000000000000000000,XInput Gamepad (GLFW),platform:Windows,a:b0,b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,back:b6,start:b7,leftstick:b8,rightstick:b9,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4,righttrigger:a5,dpup:h0.1,dpright:h0.2,dpdown:h0.4,dpleft:h0.8,
 78696e70757402000000000000000000,XInput Wheel (GLFW),platform:Windows,a:b0,b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,back:b6,start:b7,leftstick:b8,rightstick:b9,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4,righttrigger:a5,dpup:h0.1,dpright:h0.2,dpdown:h0.4,dpleft:h0.8,
