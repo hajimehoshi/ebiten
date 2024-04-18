21a906bc82fd8494df6e3c9f9ad763cb065064db
Hajime Hoshi <hajimehoshi@gmail.com>
2024-04-18 14:15:54 +0900

    internal/gamepaddb: refactoring: GLFW gamepads are requried only for Windows
    Updates #2964

--- internal/gamepaddb/gen.go
+++ internal/gamepaddb/gen.go
@@ -66,11 +66,27 @@ import (
 //go:embed gamecontrollerdb_{{.FileNameSuffix}}.txt
 var controllerBytes []byte
 
+{{if .HasGLFWGamepads}}
+var additionalGLFWGamepads = []byte(` + "`" + `
+78696e70757401000000000000000000,XInput Gamepad (GLFW),platform:Windows,a:b0,b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,back:b6,start:b7,leftstick:b8,rightstick:b9,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4,righttrigger:a5,dpup:h0.1,dpright:h0.2,dpdown:h0.4,dpleft:h0.8,
+78696e70757402000000000000000000,XInput Wheel (GLFW),platform:Windows,a:b0,b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,back:b6,start:b7,leftstick:b8,rightstick:b9,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4,righttrigger:a5,dpup:h0.1,dpright:h0.2,dpdown:h0.4,dpleft:h0.8,
+78696e70757403000000000000000000,XInput Arcade Stick (GLFW),platform:Windows,a:b0,b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,back:b6,start:b7,leftstick:b8,rightstick:b9,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4,righttrigger:a5,dpup:h0.1,dpright:h0.2,dpdown:h0.4,dpleft:h0.8,
+78696e70757404000000000000000000,XInput Flight Stick (GLFW),platform:Windows,a:b0,b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,back:b6,start:b7,leftstick:b8,rightstick:b9,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4,righttrigger:a5,dpup:h0.1,dpright:h0.2,dpdown:h0.4,dpleft:h0.8,
+78696e70757405000000000000000000,XInput Dance Pad (GLFW),platform:Windows,a:b0,b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,back:b6,start:b7,leftstick:b8,rightstick:b9,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4,righttrigger:a5,dpup:h0.1,dpright:h0.2,dpdown:h0.4,dpleft:h0.8,
+78696e70757406000000000000000000,XInput Guitar (GLFW),platform:Windows,a:b0,b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,back:b6,start:b7,leftstick:b8,rightstick:b9,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4,righttrigger:a5,dpup:h0.1,dpright:h0.2,dpdown:h0.4,dpleft:h0.8,
+78696e70757408000000000000000000,XInput Drum Kit (GLFW),platform:Windows,a:b0,b:b1,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,back:b6,start:b7,leftstick:b8,rightstick:b9,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4,righttrigger:a5,dpup:h0.1,dpright:h0.2,dpdown:h0.4,dpleft:h0.8,
+` + "`" + `)
+{{end}}
+
 func init() {
 	if err := Update(controllerBytes); err != nil {
 		panic(err)
-	}
-}`
+	}{{if .HasGLFWGamepads}}
+	if err := Update(additionalGLFWGamepads); err != nil {
+		panic(err)
+	}{{end}}
+}
+`
 
 func main() {
 	if err := run(); err != nil {
@@ -86,12 +102,14 @@ func run() error {
 	type gamePadPlatform struct {
 		filenameSuffix   string
 		buildConstraints string
+		hasGLFWGamepads  bool
 	}
 
 	platforms := map[string]gamePadPlatform{
 		"Windows": {
 			filenameSuffix:   "windows",
 			buildConstraints: "//go:build !microsoftgdk",
+			hasGLFWGamepads:  true,
 		},
 		"Mac OS X": {
 			filenameSuffix:   "macos_darwin",
@@ -142,11 +160,13 @@ func run() error {
 			DoNotEdit        string
 			BuildConstraints string
 			FileNameSuffix   string
+			HasGLFWGamepads  bool
 		}{
 			License:          license,
 			DoNotEdit:        doNotEdit,
 			BuildConstraints: platform.buildConstraints,
 			FileNameSuffix:   platform.filenameSuffix,
+			HasGLFWGamepads:  platform.hasGLFWGamepads,
 		}); err != nil {
 			return err
 		}
