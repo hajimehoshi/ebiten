657e04d3d142f4fb0c9cfa50e99a7d05f88f19ed
Hajime Hoshi <hajimehoshi@gmail.com>
2024-05-19 18:01:58 +0900

    internal/graphicsdriver/playstation5: separate the shader part

--- internal/graphicsdriver/playstation5/graphics_playstation5.go
+++ internal/graphicsdriver/playstation5/graphics_playstation5.go
@@ -153,7 +153,3 @@ func (s *Shader) ID() graphicsdriver.ShaderID {
 func (s *Shader) Dispose() {
 	C.ebitengine_DisposeShader(C.int(s.id))
 }
-
-func RegisterPrecompiledShaders(hash shaderir.SourceHash, vertex, pixel []byte) {
-	// TODO: Implement this.
-}
