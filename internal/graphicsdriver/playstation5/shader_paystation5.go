1b6a615adb9d949c2be1adb4315504aa9cce5616
Hajime Hoshi <hajimehoshi@gmail.com>
2024-09-29 17:46:52 +0900

    internal/graphicsdriver/playstation5: add Shader

---
+++ internal/graphicsdriver/playstation5/shader_paystation5.go
@@ -0,0 +1,43 @@
+// Copyright 2024 The Ebitengine Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build playstation5
+
+package playstation5
+
+import (
+	"github.com/hajimehoshi/ebiten/v2/internal/shaderir"
+)
+
+var (
+	// precompiledShaders is initialized by an automatically generated Go file.
+	precompiledShaders []*shader
+)
+
+type shader struct {
+	hash         shaderir.SourceHash
+	vertexHeader []byte
+	vertexText   []byte
+	pixelHeader  []byte
+	pixelText    []byte
+}
+
+func findShaderIndex(hash shaderir.SourceHash) (int, bool) {
+	for i, s := range precompiledShaders {
+		if s.hash == hash {
+			return i, true
+		}
+	}
+	return 0, false
+}
