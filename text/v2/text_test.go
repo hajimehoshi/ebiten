hajimehoshi/ebiten

commit 999c64cb7867c1383e77f4a6e7f66f593dc53434

text/v2: refactoring

text/v2/gotextfacesource.go
@@ -89,6 +89,16 @@ func toFontResource(source io.Reader) (font.Resource, error) {
 	return bytes.NewReader(bs), nil
 }
 
+func newGoTextFaceSource(face *font.Face) *GoTextFaceSource {
+	s := &GoTextFaceSource{
+		f: face,
+	}
+	s.addr = s
+	s.metadata = metadataFromFace(face)
+	s.outputCache = newCache[goTextOutputCacheKey, goTextOutputCacheValue](512)
+	return s
+}
+
 // NewGoTextFaceSource parses an OpenType or TrueType font and returns a GoTextFaceSource object.
 func NewGoTextFaceSource(source io.Reader) (*GoTextFaceSource, error) {
 	src, err := toFontResource(source)
@@ -106,13 +116,7 @@ func NewGoTextFaceSource(source io.Reader) (*GoTextFaceSource, error) {
 		return nil, err
 	}
 
-	s := &GoTextFaceSource{
-		f: font.NewFace(f),
-	}
-	s.addr = s
-	s.metadata = metadataFromLoader(l)
-	s.outputCache = newCache[goTextOutputCacheKey, goTextOutputCacheValue](512)
-
+	s := newGoTextFaceSource(&font.Face{Font: f})
 	return s, nil
 }
 
@@ -134,11 +138,7 @@ func NewGoTextFaceSourcesFromCollection(source io.Reader) ([]*GoTextFaceSource, 
 		if err != nil {
 			return nil, err
 		}
-		s := &GoTextFaceSource{
-			f: &font.Face{Font: f},
-		}
-		s.addr = s
-		s.metadata = metadataFromLoader(l)
+		s := newGoTextFaceSource(&font.Face{Font: f})
 		sources[i] = s
 	}
 	return sources, nil

text/v2/metadata.go
@@ -16,7 +16,6 @@ package text
 
 import (
 	"github.com/go-text/typesetting/font"
-	"github.com/go-text/typesetting/font/opentype"
 )
 
 // Metadata represents a font face's metadata.
@@ -27,8 +26,8 @@ type Metadata struct {
 	Stretch Stretch
 }
 
-func metadataFromLoader(l *opentype.Loader) Metadata {
-	d, _ := font.Describe(l, nil)
+func metadataFromFace(f *font.Face) Metadata {
+	d := f.Describe()
 	return Metadata{
 		Family:  d.Family,
 		Style:   Style(d.Aspect.Style),

commit 88a2c14a8edc5b97107288dc76cac085e5c6b15e

text/v2: add a test to parse a collection file
This test failed at c985e9bdc6c992967efd93bbebc639ab9e76aa12, but
fixed at 999c64cb7867c1383e77f4a6e7f66f593dc53434 accidentally.

This issue was introduced in 2.9 (maybe d19a774316b41a34b2ab1882b9fdcabc008ef24e),
so this doesn't have to be cherry-picked.

text/v2/text_test.go
@@ -448,3 +448,30 @@ func TestGoXFaceMetrics(t *testing.T) {
 		})
 	}
 }
+
+func TestCollection(t *testing.T) {
+	fontFilePaths := []string{
+		// If a font file doesn't exist, the test is skipped.
+		"/System/Library/Fonts/Helvetica.ttc",
+	}
+	for _, path := range fontFilePaths {
+		path := path
+		t.Run(path, func(t *testing.T) {
+			bs, err := os.ReadFile(path)
+			if err != nil {
+				t.Skipf("skipping: failed to read %s", path)
+			}
+			fs, err := text.NewGoTextFaceSourcesFromCollection(bytes.NewBuffer(bs))
+			if err != nil {
+				t.Fatal(err)
+			}
+			for _, f := range fs {
+				dst := ebiten.NewImage(16, 16)
+				text.Draw(dst, "a", &text.GoTextFace{
+					Source: f,
+					Size:   16,
+				}, nil)
+			}
+		})
+	}
+}