d359ae697b6af17c8071442aa2c4416e73e63bce
Hajime Hoshi <hajimehoshi@gmail.com>
2025-03-23 19:00:04 +0900

    examples/hsv: use debugui
    Updates #3068

--- examples/hsv/main.go
+++ examples/hsv/main.go
@@ -16,17 +16,14 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"image"
 	_ "image/jpeg"
 	"log"
-	"math"
 
+	"github.com/ebitengine/debugui"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/colorm"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 const (
@@ -38,62 +35,45 @@ var (
 	gophersImage *ebiten.Image
 )
 
-// clamp clamps v to the range [min, max].
-func clamp(v, min, max int) int {
-	if min > max {
-		panic("min must <= max")
-	}
-	if v < min {
-		return min
-	}
-	if max < v {
-		return max
-	}
-	return v
-}
-
 type Game struct {
-	hue128        int
-	saturation128 int
-	value128      int
+	debugui debugui.DebugUI
 
-	inverted bool
+	hue        float64
+	saturation float64
+	value      float64
+	inverted   bool
 }
 
 func NewGame() *Game {
 	return &Game{
-		saturation128: 128,
-		value128:      128,
+		saturation: 1,
+		value:      1,
 	}
 }
 
 func (g *Game) Update() error {
-	// Adjust HSV values along with the user's input.
-	if ebiten.IsKeyPressed(ebiten.KeyQ) {
-		g.hue128--
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		g.hue128++
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		g.saturation128--
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		g.saturation128++
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyZ) {
-		g.value128--
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyX) {
-		g.value128++
-	}
-
-	g.hue128 = clamp(g.hue128, -256, 256)
-	g.saturation128 = clamp(g.saturation128, 0, 256)
-	g.value128 = clamp(g.value128, 0, 256)
-
-	if inpututil.IsKeyJustPressed(ebiten.KeyI) {
-		g.inverted = !g.inverted
+	if err := g.debugui.Update(func(ctx *debugui.Context) error {
+		ctx.Window("HSV", image.Rect(10, 10, 260, 160), func(layout debugui.ContainerLayout) {
+			ctx.SetGridLayout([]int{-1, -2}, nil)
+			ctx.Text("Hue")
+			ctx.SliderF(&g.hue, -4, 4, 0.01, 2)
+			ctx.Text("Saturation")
+			ctx.SliderF(&g.saturation, 0, 2, 0.01, 2)
+			ctx.Text("Value")
+			ctx.SliderF(&g.value, 0, 2, 0.01, 2)
+			ctx.Text("Inverted")
+			ctx.Checkbox(&g.inverted, "")
+			ctx.Text("Reset")
+			if ctx.Button("Reset") {
+				g.hue = 0
+				g.saturation = 1
+				g.value = 1
+				g.inverted = false
+			}
+		})
+		return nil
+	}); err != nil {
+		return err
 	}
 	return nil
 }
@@ -107,11 +87,8 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	op.GeoM.Translate(float64(screenWidth)/2, float64(screenHeight)/2)
 
 	// Change HSV.
-	hue := float64(g.hue128) * 2 * math.Pi / 128
-	saturation := float64(g.saturation128) / 128
-	value := float64(g.value128) / 128
 	var c colorm.ColorM
-	c.ChangeHSV(hue, saturation, value)
+	c.ChangeHSV(g.hue, g.saturation, g.value)
 
 	// Invert the color.
 	if g.inverted {
@@ -121,16 +98,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	colorm.DrawImage(screen, gophersImage, c, op)
 
-	// Draw the text of the current status.
-	msgInverted := "false"
-	if g.inverted {
-		msgInverted = "true"
-	}
-	msg := fmt.Sprintf(`Hue:        %0.2f [Q][W]
-Saturation: %0.2f [A][S]
-Value:      %0.2f [Z][X]
-Inverted:   %s [I]`, hue, saturation, value, msgInverted)
-	ebitenutil.DebugPrint(screen, msg)
+	g.debugui.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
