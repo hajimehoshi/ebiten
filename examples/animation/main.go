010869e14b7ed33cfbb59b1a3aa085a025cdba9c
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-08-08 16:42:41 +0200

    Animation - redirecting to local image frames

--- examples/animation/main.go
+++ examples/animation/main.go
@@ -1,27 +1,12 @@
-// Copyright 2018 The Ebiten Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
-	"bytes"
 	"image"
 	_ "image/png"
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
 )
 
 const (
@@ -62,8 +47,13 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
-	// Decode an image from the image file's byte slice.
-	img, _, err := image.Decode(bytes.NewReader(images.Runner_png))
+	file, err := os.Open("../resources/images/runner.png")
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer file.Close()
+
+	img, _, err := image.Decode(file)
 	if err != nil {
 		log.Fatal(err)
 	}
