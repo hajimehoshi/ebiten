042d206e4f112809779cf4b9218619ef59742b0b
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-08-08 17:11:41 +0200

    redirecting 2

--- examples/mosaic/main.go
+++ examples/mosaic/main.go
@@ -1,27 +1,12 @@
-// Copyright 2014 Hajime Hoshi
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
-	"bytes"
+	"os"
 	"image"
 	_ "image/jpeg"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
 )
 
 const (
@@ -36,8 +21,13 @@ var (
 )
 
 func init() {
-	// Decode an image from the image file's byte slice.
-	img, _, err := image.Decode(bytes.NewReader(images.Gophers_jpg))
+		file, err := os.Open("../resources/images/gophers.jpg")
+            if err != nil {
+                log.Fatal(err)
+            }
+            defer file.Close()
+
+    	img, _, err := image.Decode(file)
 	if err != nil {
 		log.Fatal(err)
 	}
