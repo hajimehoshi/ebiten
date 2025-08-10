6a41a820a4c1e502eaa090fa1a0dbf51e222d047
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2025-08-10 17:31:31 +0900

    internal: Prefix panic messages with 'directx: not implemented'
    Updated all panic messages in DirectX stub functions to include the 'directx:' prefix for clearer error context. This improves debugging by indicating the source of the unimplemented functionality.

--- internal/graphicsdriver/directx/api_notmicrosoftgdk_windows.go
+++ internal/graphicsdriver/directx/api_notmicrosoftgdk_windows.go
@@ -25,101 +25,101 @@ func _D3D12_RESOURCE_STATE_PRESENT() _D3D12_RESOURCE_STATES {
 }
 
 func _ID3D12CommandQueue_ExecuteCommandLists(i *_ID3D12CommandQueue, ppCommandLists []*_ID3D12GraphicsCommandList) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12CommandQueue_PresentX(i *_ID3D12CommandQueue, planeCount uint32, pPlaneParameters *_D3D12XBOX_PRESENT_PLANE_PARAMETERS, pPresentParameters *_D3D12XBOX_PRESENT_PARAMETERS) uintptr {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12CommandQueue_Release(i *_ID3D12CommandQueue) uint32 {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12CommandQueue_ResumeX(i *_ID3D12CommandQueue) uintptr {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12CommandQueue_Signal(i *_ID3D12CommandQueue, pFence *_ID3D12Fence, value uint64) uintptr {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12CommandQueue_SuspendX(i *_ID3D12CommandQueue, flags uint32) uintptr {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_ClearDepthStencilView(i *_ID3D12GraphicsCommandList, depthStencilView _D3D12_CPU_DESCRIPTOR_HANDLE, clearFlags _D3D12_CLEAR_FLAGS, depth float32, stencil uint8, rects []_D3D12_RECT) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_ClearRenderTargetView(i *_ID3D12GraphicsCommandList, pRenderTargetView _D3D12_CPU_DESCRIPTOR_HANDLE, colorRGBA [4]float32, rects []_D3D12_RECT) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_Close(i *_ID3D12GraphicsCommandList) uintptr {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_CopyTextureRegion(i *_ID3D12GraphicsCommandList, pDst unsafe.Pointer, dstX uint32, dstY uint32, dstZ uint32, pSrc unsafe.Pointer, pSrcBox *_D3D12_BOX) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_DrawIndexedInstanced(i *_ID3D12GraphicsCommandList, indexCountPerInstance uint32, instanceCount uint32, startIndexLocation uint32, baseVertexLocation int32, startInstanceLocation uint32) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_IASetIndexBuffer(i *_ID3D12GraphicsCommandList, pView *_D3D12_INDEX_BUFFER_VIEW) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_IASetPrimitiveTopology(i *_ID3D12GraphicsCommandList, primitiveTopology _D3D_PRIMITIVE_TOPOLOGY) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_IASetVertexBuffers(i *_ID3D12GraphicsCommandList, startSlot uint32, pViews []_D3D12_VERTEX_BUFFER_VIEW) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_OMSetRenderTargets(i *_ID3D12GraphicsCommandList, renderTargetDescriptors []_D3D12_CPU_DESCRIPTOR_HANDLE, rtsSingleHandleToDescriptorRange bool, pDepthStencilDescriptor *_D3D12_CPU_DESCRIPTOR_HANDLE) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_OMSetStencilRef(i *_ID3D12GraphicsCommandList, stencilRef uint32) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_Release(i *_ID3D12GraphicsCommandList) uint32 {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_Reset(i *_ID3D12GraphicsCommandList, pAllocator *_ID3D12CommandAllocator, pInitialState *_ID3D12PipelineState) uintptr {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_ResourceBarrier(i *_ID3D12GraphicsCommandList, barriers []_D3D12_RESOURCE_BARRIER_Transition) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_RSSetViewports(i *_ID3D12GraphicsCommandList, viewports []_D3D12_VIEWPORT) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_RSSetScissorRects(i *_ID3D12GraphicsCommandList, rects []_D3D12_RECT) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_SetDescriptorHeaps(i *_ID3D12GraphicsCommandList, descriptorHeaps []*_ID3D12DescriptorHeap) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_SetGraphicsRootDescriptorTable(i *_ID3D12GraphicsCommandList, rootParameterIndex uint32, baseDescriptor _D3D12_GPU_DESCRIPTOR_HANDLE) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_SetGraphicsRootSignature(i *_ID3D12GraphicsCommandList, pRootSignature *_ID3D12RootSignature) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
 
 func _ID3D12GraphicsCommandList_SetPipelineState(i *_ID3D12GraphicsCommandList, pPipelineState *_ID3D12PipelineState) {
-	panic("not implemented")
+	panic("directx: not implemented")
 }
