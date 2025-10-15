7ecd4037a493b3c1ba6b41de20d70c5ae76a8215
Hajime Hoshi <hajimehoshi@gmail.com>
2025-10-15 19:49:58 +0900

    vector: bug fix: FillRect or other utility functions on a sub-image didn't work correctly
    Closes #3330

--- vector/util_test.go
+++ vector/util_test.go
@@ -15,6 +15,7 @@
 package vector_test
 
 import (
+	"image"
 	"image/color"
 	"testing"
 
@@ -41,6 +42,101 @@ func TestStrokeRectAntiAlias(t *testing.T) {
 	dst := ebiten.NewImage(16, 16)
 	vector.StrokeRect(dst, 0, 0, 16, 16, 2, color.White, true)
 	if got, want := dst.At(5, 5), (color.RGBA{}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+}
+
+// Issue #3330
+func TestFillRectSubImage(t *testing.T) {
+	dst := ebiten.NewImage(16, 16)
+
+	dst2 := dst.SubImage(image.Rect(0, 0, 8, 8)).(*ebiten.Image)
+	vector.FillRect(dst2, 0, 0, 8, 8, color.White, true)
+	if got, want := dst.At(5, 5), (color.RGBA{0xff, 0xff, 0xff, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+	if got, want := dst2.At(5, 5), (color.RGBA{0xff, 0xff, 0xff, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+
+	dst3 := dst2.SubImage(image.Rect(4, 4, 8, 8)).(*ebiten.Image)
+	vector.FillRect(dst3, 4, 4, 4, 4, color.Black, true)
+	if got, want := dst.At(5, 5), (color.RGBA{0x00, 0x00, 0x00, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+	if got, want := dst2.At(5, 5), (color.RGBA{0x00, 0x00, 0x00, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+	if got, want := dst3.At(5, 5), (color.RGBA{0x00, 0x00, 0x00, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+}
+
+// Issue #3330
+func TestFillCircleSubImage(t *testing.T) {
+	dst := ebiten.NewImage(16, 16)
+
+	dst2 := dst.SubImage(image.Rect(0, 0, 8, 8)).(*ebiten.Image)
+	vector.FillCircle(dst2, 4, 4, 4, color.White, true)
+	if got, want := dst.At(5, 5), (color.RGBA{0xff, 0xff, 0xff, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+	if got, want := dst2.At(5, 5), (color.RGBA{0xff, 0xff, 0xff, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+
+	dst3 := dst2.SubImage(image.Rect(4, 4, 8, 8)).(*ebiten.Image)
+	vector.FillCircle(dst3, 6, 6, 4, color.Black, true)
+	if got, want := dst.At(5, 5), (color.RGBA{0x00, 0x00, 0x00, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+	if got, want := dst2.At(5, 5), (color.RGBA{0x00, 0x00, 0x00, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+	if got, want := dst3.At(5, 5), (color.RGBA{0x00, 0x00, 0x00, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+}
+
+// Issue #3330
+func TestFillPathSubImage(t *testing.T) {
+	dst := ebiten.NewImage(16, 16)
+
+	dst2 := dst.SubImage(image.Rect(0, 0, 8, 8)).(*ebiten.Image)
+	var p vector.Path
+	p.MoveTo(0, 0)
+	p.LineTo(8, 0)
+	p.LineTo(8, 8)
+	p.LineTo(0, 8)
+	p.Close()
+	op := &vector.DrawPathOptions{}
+	op.ColorScale.ScaleWithColor(color.White)
+	op.AntiAlias = true
+	vector.FillPath(dst2, &p, nil, op)
+	if got, want := dst.At(5, 5), (color.RGBA{0xff, 0xff, 0xff, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+	if got, want := dst2.At(5, 5), (color.RGBA{0xff, 0xff, 0xff, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+
+	dst3 := dst2.SubImage(image.Rect(4, 4, 8, 8)).(*ebiten.Image)
+	var p2 vector.Path
+	p2.MoveTo(4, 4)
+	p2.LineTo(8, 4)
+	p2.LineTo(8, 8)
+	p2.LineTo(4, 8)
+	p2.Close()
+	op.ColorScale.Reset()
+	op.ColorScale.ScaleWithColor(color.Black)
+	vector.FillPath(dst3, &p2, nil, op)
+	if got, want := dst.At(5, 5), (color.RGBA{0x00, 0x00, 0x00, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+	if got, want := dst2.At(5, 5), (color.RGBA{0x00, 0x00, 0x00, 0xff}); got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+	if got, want := dst3.At(5, 5), (color.RGBA{0x00, 0x00, 0x00, 0xff}); got != want {
 		t.Errorf("got: %v, want: %v", got, want)
 	}
 }
