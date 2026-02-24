28a8f8bfe3007032c134596115366bd16407035d
Hajime Hoshi <hajimehoshi@gmail.com>
2026-02-24 23:45:54 +0900

    internal/gamepad: fix typo
    Updates #2014
    Updates #2015

--- internal/gamepad/gamepad_darwin.go
+++ internal/gamepad/gamepad_darwin.go
@@ -1,4 +1,4 @@
-// Copyright 2025 The Ebiten Authors
+// Copyright 2026 The Ebitengine Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
