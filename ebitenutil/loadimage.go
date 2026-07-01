47c67d8c5e74f9715139345b2afc868106822d80
Alexander peczon <97857268+BakedSoups@users.noreply.github.com>
2026-06-30 20:34:36 -0700

    ebitenutil: check HTTP status in NewImageFromURL (#3472)

--- ebitenutil/loadimage.go
+++ ebitenutil/loadimage.go
@@ -15,6 +15,7 @@
 package ebitenutil
 
 import (
+	"fmt"
 	"image"
 	"io"
 	"net/http"
@@ -47,6 +48,9 @@ func NewImageFromURL(url string) (*ebiten.Image, error) {
 	defer func() {
 		_ = res.Body.Close()
 	}()
+	if res.StatusCode < http.StatusOK || 300 <= res.StatusCode {
+		return nil, fmt.Errorf("ebitenutil: request to %s failed: %s", url, res.Status)
+	}
 
 	img, _, err := image.Decode(res.Body)
 	if err != nil {
---
+++ ebitenutil/loadimage_test.go
@@ -0,0 +1,39 @@
+// Copyright 2026 The Ebitengine Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ebitenutil_test
+
+import (
+	"image"
+	"image/png"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+func TestNewImageFromURLHTTPError(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		if err := png.Encode(w, image.NewRGBA(image.Rect(0, 0, 1, 1))); err != nil {
+			t.Errorf("png.Encode failed: %v", err)
+		}
+	}))
+	defer s.Close()
+
+	if _, err := ebitenutil.NewImageFromURL(s.URL); err == nil {
+		t.Fatal("NewImageFromURL returned nil error for HTTP 404")
+	}
+}
