16dbdb368b95723f13cdf88c8d8ee7b455bcadd6
Hiroki KUMAZAKI <hiroki.kumazaki@gmail.com>
2026-09-17 11:56:53 +0900

    ebiten: clarify DrawImage's sub-image panic behavior (#3714)
    Document that drawing between an image and its sub-images, or
    between sub-images of the same image, also panics.

--- image.go
+++ image.go
@@ -275,6 +275,8 @@ func init() {
 // When the image i is disposed and img is not, DrawImage does nothing.
 //
 // When the given image is as same as i, DrawImage panics.
+// A whole image and its sub-images, or sub-images of the same whole image,
+// are considered to be the same image, and DrawImage panics for them as well.
 //
 // DrawImage works more efficiently as batches
 // when the successive calls of DrawImage satisfy the below conditions:
