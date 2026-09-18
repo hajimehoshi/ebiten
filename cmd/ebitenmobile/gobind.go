4f049d9f8f75954001324b7484a3954b36f9e010
Hiroki KUMAZAKI <hiroki.kumazaki@gmail.com>
2026-09-19 03:18:44 +0900

    cmd/ebitenmobile: return an error for unsupported languages (#3725)
    Return an error for unsupported gobind -lang values so main reports
    them through log.Fatal without a panic stack trace. Prefix the error
    with "ebitenmobile:" to identify its source.

--- cmd/ebitenmobile/gobind.go
+++ cmd/ebitenmobile/gobind.go
@@ -140,7 +140,7 @@ import "C"`); err != nil {
 		case "go":
 			// Do nothing.
 		default:
-			panic(fmt.Sprintf("unsupported language: %s", lang))
+			return fmt.Errorf("ebitenmobile: unsupported language: %s", lang)
 		}
 	}
 
